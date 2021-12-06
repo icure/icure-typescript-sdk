e544e6839afceb3206494e74cc7864141e39f0f4
Damien Libouton <damien.libouton@medispring.be>
2021-12-06 11:53:16 +0100

    Generate icc-api from kraken-sas 2.1.41593-release.kraken.a5c8ca88d2

---
+++ icc-api/model/ModelHelper.ts
@@ -0,0 +1,17 @@
+export function b64_2ab(v: any) {
+  if (v instanceof ArrayBuffer) {
+    return v
+  }
+  if (v instanceof Uint8Array) {
+    return v.buffer
+  }
+  if (typeof v === "string") {
+    const bs = Buffer && Buffer.from(v as string, "base64").toString("ascii") || atob(v as string)
+    var data = new Uint8Array(bs.length)
+    for (let i = 0; i < bs.length; i++) {
+      data[i] = bs.charCodeAt(i)
+    }
+    return data.buffer
+  }
+  return v
+}
