2529da7294dfc7a02d52d5955069eeba7183aa56
Lana Descheemaeker <lana.descheemaeker@b12-consulting.com>
2022-01-18 14:29:48 +0100

    bugfix compilation error of medispring

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-export * as iccXApi from './icc-x-api'
-export * as iccApi from './icc-api'
-export * as iccModels from './icc-api/model/models'
+export * from './icc-x-api'
+export * from './icc-api'
+export * from './icc-api/model/models'
 
 export * from './icc-x-api/utils'
-export * as filters from './icc-x-api/filters/filters'
+export * from './icc-x-api/filters/filters'
 
 export { XHR } from './icc-api/api/XHR'
