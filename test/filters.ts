180b4ed8377a2340ef746f3b1491c8e69ce86a4b
Antoine Duchâteau <ad@taktik.com>
2022-02-24 23:37:47 +0100

    Solved several problems when logging in as patient

--- test/filters.ts
+++ test/filters.ts
@@ -2,18 +2,18 @@ import { crypto } from '../node-compat'
 import { ShamirClass } from '../icc-x-api/crypto/shamir'
 import { expect } from 'chai'
 import 'mocha'
-import { FilterFor } from '../icc-x-api/filters/filters'
+import { Filter } from '../icc-x-api/filters/filters'
 
 describe('Composition', () => {
   it('or should return an or combining other filters', () => {
-    const builder = FilterFor.patient().forHcp('me').olderThan(65).or().youngerThan(18)
+    const builder = Filter.patient().forHcp('me').olderThan(65).or().youngerThan(18)
     const filter = builder.build()
 
     expect((filter as any).$type).to.equal('UnionFilter')
   })
 
   it('and should work with or', () => {
-    const builder = FilterFor.patient().forHcp('me').olderThan(65).or().youngerThan(18).and().searchByName('dup')
+    const builder = Filter.patient().forHcp('me').olderThan(65).or().youngerThan(18).and().searchByName('dup')
     const filter = builder.build()
 
     expect((filter as any).$type).to.equal('IntersectionFilter')
@@ -21,7 +21,7 @@ describe('Composition', () => {
   })
 
   it('precedence of operators can be managed for and/or', () => {
-    const builder = FilterFor.patient()
+    const builder = Filter.patient()
       .forHcp('me')
       .olderThan(65)
       .or((it) => it.youngerThan(18).and().searchByName('dup'))
@@ -33,7 +33,7 @@ describe('Composition', () => {
   })
 
   it('and should work with and', () => {
-    const builder = FilterFor.patient().youngerThan(65).and().olderThan(18).and().searchByName('dup')
+    const builder = Filter.patient().youngerThan(65).and().olderThan(18).and().searchByName('dup')
     const filter = builder.build()
 
     expect((filter as any).$type).to.equal('IntersectionFilter')
@@ -41,7 +41,7 @@ describe('Composition', () => {
   })
 
   it('should not crash miserably on incomplete or/and', () => {
-    const builder = FilterFor.patient().youngerThan(65).and()
+    const builder = Filter.patient().youngerThan(65).and()
     const filter = builder.build()
 
     expect((filter as any).$type).to.equal('IntersectionFilter')
@@ -50,7 +50,7 @@ describe('Composition', () => {
   })
 
   it('should not crash miserably on combination of or/and', () => {
-    const builder = FilterFor.patient().youngerThan(65).and().or().olderThan(75)
+    const builder = Filter.patient().youngerThan(65).and().or().olderThan(75)
     const filter = builder.build()
 
     expect((filter as any).$type).to.equal('IntersectionFilter')
