d2cdf8fb94036d4d11b63f7c48efa2a15ce78e9e
Antoine Duchâteau <ad@taktik.com>
2022-03-08 17:03:45 +0100

    Uniformize b64 conversions, remove duplicate code and add tests

--- icc-x-api/utils/binary-utils.ts
+++ icc-x-api/utils/binary-utils.ts
@@ -1,27 +1,8 @@
-const textDecoder = TextDecoder ? new TextDecoder() : null
-const textEncoder = TextEncoder ? new TextEncoder() : null
+import { ua2string, string2ua, string2ab, ua2ab, b64_2ab, b64_2ua, ua2b64, b2a, a2b } from '../../icc-api/model/ModelHelper'
+export { ua2string, string2ua, string2ab, ua2ab, b64_2ab, b64_2ua, ua2b64, b2a, a2b }
 
-export function b2a(a: string): string {
-  if (typeof Buffer !== 'undefined') {
-    const buf = Buffer.from(a, 'latin1')
-    return buf.toString('base64')
-  }
-  if (typeof btoa !== 'undefined') {
-    return btoa(a)
-  }
-  throw new Error('Unsupported operation b2a')
-}
-
-export function a2b(s: string): string {
-  if (typeof Buffer !== 'undefined') {
-    const buf = new Buffer(s, 'base64')
-    return buf.toString('latin1')
-  }
-  if (typeof atob !== 'undefined') {
-    return atob(s)
-  }
-  throw new Error('Unsupported operation a2b')
-}
+const textDecoder = typeof TextDecoder !== 'undefined' ? new TextDecoder() : null
+const textEncoder = typeof TextEncoder !== 'undefined' ? new TextEncoder() : null
 
 export function utf8_2ua(str: string): Uint8Array {
   if (textEncoder) {
@@ -145,35 +126,6 @@ export function ua2hex(_ua: Uint8Array | ArrayBuffer): string {
   return s
 }
 
-export function string2ua(s: string): Uint8Array {
-  const ua = new Uint8Array(s.length)
-  for (let i = 0; i < s.length; i++) {
-    ua[i] = s.charCodeAt(i) & 0xff
-  }
-  return ua
-}
-
-export function string2ab(s: string): ArrayBuffer {
-  return ua2ab(string2ua(s))
-}
-
-export function ua2ab(ua: Uint8Array): ArrayBuffer {
-  const buffer = ua.buffer
-  return (buffer.byteLength > ua.byteLength ? buffer.slice(0, ua.byteLength) : buffer) as ArrayBuffer
-}
-
-export function b64_2ab(s: string): ArrayBuffer {
-  return ua2ab(string2ua(a2b(s)))
-}
-
-export function b64_2ua(s: string): Uint8Array {
-  return string2ua(a2b(s))
-}
-
-export function ua2b64(_ua: Uint8Array | ArrayBuffer): string {
-  return b2a(ua2string(_ua))
-}
-
 export function b64_2uas(s: string): Array<Uint8Array> {
   const sliceSize = 1024
   const byteCharacters = a2b(s)
@@ -192,26 +144,6 @@ export function b64_2uas(s: string): Array<Uint8Array> {
     byteArrays[sliceIndex] = new Uint8Array(bytes)
   }
   return byteArrays
-}
-
-/**
- * ArrayBuffer to String - resilient to large ArrayBuffers.
- *
- * @param _ua
- * @returns {string}
- */
-export function ua2string(_ua: Uint8Array | ArrayBuffer): string {
-  let str = ''
-  const ab = new Uint8Array(_ua)
-  const abLen = ab.length
-  const CHUNK_SIZE = Math.pow(2, 8)
-  let offset, len, subab
-  for (offset = 0; offset < abLen; offset += CHUNK_SIZE) {
-    len = Math.min(CHUNK_SIZE, abLen - offset)
-    subab = ab.subarray(offset, offset + len)
-    str += String.fromCharCode.apply(null, subab as any)
-  }
-  return str
 }
 
 export function ua2b64Url(ua: Uint8Array | ArrayBuffer): string {
