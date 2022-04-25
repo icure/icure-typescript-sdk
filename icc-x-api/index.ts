6a658d4fddf99b61ee74a892eec8d15701b62daf
Antoine Duchâteau <ad@taktik.com>
2022-04-25 17:12:08 +0200

    Add permission api in api

--- icc-x-api/index.ts
+++ icc-x-api/index.ts
@@ -1,21 +1,21 @@
-import {IccAgendaApi, IccAuthApi, IccEntityrefApi, IccGroupApi, IccInsuranceApi, IccPatientApi} from '../icc-api'
-import {IccUserXApi} from './icc-user-x-api'
-import {IccCryptoXApi} from './icc-crypto-x-api'
-import {IccContactXApi} from './icc-contact-x-api'
-import {IccInvoiceXApi} from './icc-invoice-x-api'
-import {IccDocumentXApi} from './icc-document-x-api'
-import {IccHcpartyXApi} from './icc-hcparty-x-api'
-import {IccFormXApi} from './icc-form-x-api'
-import {IccHelementXApi} from './icc-helement-x-api'
-import {IccClassificationXApi} from './icc-classification-x-api'
-import {IccCalendarItemXApi} from './icc-calendar-item-x-api'
-import {IccPatientXApi} from './icc-patient-x-api'
-import {IccMessageXApi} from './icc-message-x-api'
-import {IccReceiptXApi} from './icc-receipt-x-api'
-import {IccAccesslogXApi} from './icc-accesslog-x-api'
-import {IccTimeTableXApi} from './icc-time-table-x-api'
-import {IccDeviceApi} from '../icc-api/api/IccDeviceApi'
-import {IccCodeXApi} from './icc-code-x-api'
+import { IccAgendaApi, IccAuthApi, IccEntityrefApi, IccGroupApi, IccInsuranceApi, IccPatientApi, IccPermissionApi } from '../icc-api'
+import { IccUserXApi } from './icc-user-x-api'
+import { IccCryptoXApi } from './icc-crypto-x-api'
+import { IccContactXApi } from './icc-contact-x-api'
+import { IccInvoiceXApi } from './icc-invoice-x-api'
+import { IccDocumentXApi } from './icc-document-x-api'
+import { IccHcpartyXApi } from './icc-hcparty-x-api'
+import { IccFormXApi } from './icc-form-x-api'
+import { IccHelementXApi } from './icc-helement-x-api'
+import { IccClassificationXApi } from './icc-classification-x-api'
+import { IccCalendarItemXApi } from './icc-calendar-item-x-api'
+import { IccPatientXApi } from './icc-patient-x-api'
+import { IccMessageXApi } from './icc-message-x-api'
+import { IccReceiptXApi } from './icc-receipt-x-api'
+import { IccAccesslogXApi } from './icc-accesslog-x-api'
+import { IccTimeTableXApi } from './icc-time-table-x-api'
+import { IccDeviceApi } from '../icc-api/api/IccDeviceApi'
+import { IccCodeXApi } from './icc-code-x-api'
 
 export * from './icc-accesslog-x-api'
 export * from './icc-bekmehr-x-api'
@@ -62,6 +62,7 @@ export const Api = function (
   const codeApi = new IccCodeXApi(host, headers, fetchImpl)
   const entityReferenceApi = new IccEntityrefApi(host, headers, fetchImpl)
   const userApi = new IccUserXApi(host, headers, fetchImpl)
+  const permissionApi = new IccPermissionApi(host, headers, fetchImpl)
   const healthcarePartyApi = new IccHcpartyXApi(host, headers, fetchImpl)
   const deviceApi = new IccDeviceApi(host, headers, fetchImpl)
   const cryptoApi = new IccCryptoXApi(host, headers, healthcarePartyApi, new IccPatientApi(host, headers, fetchImpl), deviceApi, crypto)
@@ -101,6 +102,7 @@ export const Api = function (
     authApi,
     codeApi,
     userApi,
+    permissionApi,
     patientApi,
     healthcarePartyApi,
     deviceApi,
