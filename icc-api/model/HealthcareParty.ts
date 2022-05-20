06ddfaaaf34a3762f120241c9dcef92f9d11d5b9
Antoine Duchâteau <ad@taktik.com>
2022-05-20 18:09:22 +0200

    Updated icc-api to latest version of kraken

--- icc-api/model/HealthcareParty.ts
+++ icc-api/model/HealthcareParty.ts
@@ -14,6 +14,7 @@ import { CodeStub } from './CodeStub'
 import { FinancialInstitutionInformation } from './FinancialInstitutionInformation'
 import { FlatRateTarification } from './FlatRateTarification'
 import { HealthcarePartyHistoryStatus } from './HealthcarePartyHistoryStatus'
+import { Identifier } from './Identifier'
 import { PersonName } from './PersonName'
 import { PropertyStub } from './PropertyStub'
 
@@ -46,6 +47,18 @@ export class HealthcareParty {
    * hard delete (unix epoch in ms) timestamp of the object. Filled automatically when deletePatient is called.
    */
   deletionDate?: number
+  /**
+   * The healthcareparty's identifiers, used by the client to identify uniquely and unambiguously the HCP. However, iCure may not guarantee this uniqueness by itself : This should be done at the client side.
+   */
+  identifier?: Array<Identifier>
+  /**
+   * A tag is an item from a codification system that qualifies an entity as being member of a certain class, whatever the value it might have taken. If the tag qualifies the content of a field, it means that whatever the content of the field, the tag will always apply. For example, the label of a field is qualified using a tag. LOINC is a codification system typically used for tags.
+   */
+  tags?: Array<CodeStub>
+  /**
+   * A code is an item from a codification system that qualifies the content of this entity. SNOMED-CT, ICPC-2 or ICD-10 codifications systems can be used for codes
+   */
+  codes?: Array<CodeStub>
   /**
    * The full name of the healthcare party, used mainly when the healthcare party is an organization
    */
@@ -171,21 +184,17 @@ export class HealthcareParty {
   options?: { [key: string]: string }
   properties?: Array<PropertyStub>
   /**
-   * For each couple of HcParties (delegator and delegate), this map contains the exchange AES key. The delegator is always this hcp, the key of the map is the id of the delegate. The AES exchange key is encrypted using RSA twice : once using this hcp public key (index 0 in the Array) and once using the other hcp public key (index 1 in the Array). For a pair of HcParties. Each HcParty always has one AES exchange key for himself.
+   * For each couple of HcParties (delegator and delegate), this map contains the exchange AES key. The delegator is always this hcp, the key of the map is the id of the delegate.The AES exchange key is encrypted using RSA twice : once using this hcp public key (index 0 in the Array) and once using the other hcp public key (index 1 in the Array). For a pair of HcParties. Each HcParty always has one AES exchange key for himself.
    */
   hcPartyKeys?: { [key: string]: Array<string> }
   /**
-   * Extra AES exchange keys, usually the ones we lost access to at some point. The structure is { publicKey: { delegateId: [aesExKey_for_this, aesExKey_for_delegate] } }
+   * Extra AES exchange keys, usually the ones we lost access to at some point. The structure is { publicKey: { delegateId: { myPubKey1: aesExKey_for_this, delegatePubKey1: aesExKey_for_delegate } } }
    */
-  aesExchangeKeys?: { [key: string]: { [key: string]: Array<string> } }
+  aesExchangeKeys?: { [key: string]: { [key: string]: { [key: string]: string } } }
   /**
    * Our private keys encrypted with our public keys. The structure is { publicKey1: { publicKey2: privateKey2_encrypted_with_publicKey1, publicKey3: privateKey3_encrypted_with_publicKey1 } }
    */
   transferKeys?: { [key: string]: { [key: string]: string } }
-  /**
-   * The hcparty keys (first of the pair) for which we are asking a re-encryption by the delegate using our new publicKey.
-   */
-  lostHcPartyKeys?: Array<string>
   /**
    * The privateKeyShamirPartitions are used to share this hcp's private RSA key with a series of other hcParties using Shamir's algorithm. The key of the map is the hcp Id with whom this partition has been shared. The value is \"threshold⎮partition in hex\" encrypted using the the partition's holder's public RSA key
    */
