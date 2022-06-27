c94322a8e9ab216019800a4f5b4c3dcc7f719cf7
lisatr2812 <lisa.trapletti@gmail.com>
2022-06-27 14:37:49 +0200

    Sending Maintenance Tasks when adding a new KeyPair

--- icc-x-api/utils/functional-util.ts
+++ icc-x-api/utils/functional-util.ts
@@ -2,3 +2,9 @@ export function also<T>(value: T, lambda: (it: T) => void) {
   lambda(value)
   return value
 }
+
+export function fold<T, R>(whereToFold: Array<T>, initial: R, operation: (acc: R, element: T) => R): R {
+  let accumulator = initial
+  for (let i= 0; i<=whereToFold.length; i++) accumulator = operation(accumulator, whereToFold[i])
+  return accumulator
+}
