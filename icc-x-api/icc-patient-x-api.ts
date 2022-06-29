6ba8f7587492dc497ea43381dddbd358d71c8aca
Sebastien <sebastien.janssens@b12-consulting.com>
2022-06-29 14:45:31 +0200

    do not crash on empty service contents

--- icc-x-api/icc-patient-x-api.ts
+++ icc-x-api/icc-patient-x-api.ts
@@ -814,7 +814,7 @@ export class IccPatientXApi extends IccPatientApi {
                   ctcs.forEach(
                     (c: models.Contact) =>
                       c.services &&
-                      c.services.forEach((s) => s.content && Object.values(s.content).forEach((c) => c.documentId && (docIds[c.documentId] = 1)))
+                      c.services.forEach((s) => s.content && Object.values(s.content).forEach((c) => c && c.documentId && (docIds[c.documentId] = 1)))
                   )
 
                   return retry(() => this.documentApi.getDocuments(new ListOfIds({ ids: Object.keys(docIds) }))).then((docs: Array<Document>) => {
@@ -1122,7 +1122,7 @@ export class IccPatientXApi extends IccPatientApi {
                   ctcs.forEach(
                     (c: models.Contact) =>
                       c.services &&
-                      c.services.forEach((s) => s.content && Object.values(s.content).forEach((c) => c.documentId && (docIds[c.documentId] = 1)))
+                      c.services.forEach((s) => s.content && Object.values(s.content).forEach((c) => c && c.documentId && (docIds[c.documentId] = 1)))
                   )
 
                   return retry(() => this.documentApi.getDocuments(new ListOfIds({ ids: Object.keys(docIds) }))).then((docs: Array<Document>) => {
