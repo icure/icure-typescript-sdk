02e989fd0db6c9602520611f4c992a0f8635d3f9
Antoine Duchâteau <ad@taktik.com>
2022-07-04 13:33:38 +0200

    Fix bug in fold

--- icc-x-api/utils/functional-util.ts
+++ icc-x-api/utils/functional-util.ts
@@ -5,6 +5,6 @@ export function also<T>(value: T, lambda: (it: T) => void) {
 
 export function fold<T, R>(whereToFold: Array<T>, initial: R, operation: (acc: R, element: T) => R): R {
   let accumulator = initial
-  for (let i= 0; i<=whereToFold.length; i++) accumulator = operation(accumulator, whereToFold[i])
+  for (let i = 0; i < whereToFold.length; i++) accumulator = operation(accumulator, whereToFold[i])
   return accumulator
 }
