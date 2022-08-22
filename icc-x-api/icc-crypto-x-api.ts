6734d158ac044b2c5d61b44cf1b09fa239e413a8
Sebastien <sebastien.janssens@b12-consulting.com>
2022-08-22 17:26:46 +0200

    new function to check if dataowner can decrypt patient to solve error thrown when we attempt to encrypt a patient that only the parent can decrypt

--- icc-x-api/icc-crypto-x-api.ts
+++ icc-x-api/icc-crypto-x-api.ts
@@ -1459,6 +1459,17 @@ export class IccCryptoXApi {
           .catch(() => null)
   }
 
+
+  async dataOwnerCanDecryptPatient(dataOwnerId: string, p: models.Patient): Promise<boolean> {
+    if (p.encryptionKeys && p.encryptionKeys[dataOwnerId]?.length)
+      return true
+
+    const dataOwner = (await this.getDataOwner(dataOwnerId))
+    if (dataOwner.type === 'hcp' && p.encryptionKeys && p.encryptionKeys[dataOwner.dataOwner.parentId!]?.length) return true
+
+    return false
+  }
+
   async encryptDecrypt(
     method: 'encrypt' | 'decrypt',
     content: Uint8Array | ArrayBuffer,
