f262ea331c07972e12d23c57b13014f0f3e1c2a4
Clément VANDENDAELEN <cv@icure.com>
2022-10-18 15:33:03 +0200

    Rollback keychain storage changes

--- icc-x-api/storage/KeyStorageImpl.ts
+++ icc-x-api/storage/KeyStorageImpl.ts
@@ -34,19 +34,4 @@ export class KeyStorageImpl implements KeyStorageFacade {
   storeKeyPair(key: string, keyPair: { publicKey: JsonWebKey; privateKey: JsonWebKey }): void {
     return this._storage.setItem(key, JSON.stringify(keyPair))
   }
-
-  storeKeychain(key: string, keychain: number | string): void {
-    const handlers = {
-      number: (keychainNumber: number) =>
-        this._storage.setItem(key, b2a(new Uint8Array(keychainNumber).reduce((data, byte) => data + String.fromCharCode(byte), ''))),
-      string: (keychainB64: string) => this._storage.setItem(key, keychainB64),
-    }
-
-    // @ts-ignore
-    handlers[typeof keychain](keychain)
-  }
-
-  getKeychain(key: string): string | undefined {
-    return this._storage.getItem(key)
-  }
 }
