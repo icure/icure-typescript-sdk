d925cae5ebd5136d634b726890d2bdc5d33915d0
Clément VANDENDAELEN <cv@icure.com>
2022-11-03 15:40:49 +0100

    Added missing headers for getToken request

--- icc-api/api/IccUserApi.ts
+++ icc-api/api/IccUserApi.ts
@@ -308,7 +308,7 @@ export class IccUserApi {
       '?ts=' +
       new Date().getTime() +
       (tokenValidity ? '&tokenValidity=' + encodeURIComponent(String(tokenValidity)) : '')
-    let headers = this.headers
+    let headers = this.headers.concat(new XHR.Header('Accept', '*'), new XHR.Header('Content-type', 'application/json'))
     token && (headers = headers.concat(new XHR.Header('token', token)))
     return XHR.sendCommand('POST', _url, headers, _body, this.fetchImpl)
       .then((doc) => JSON.parse(JSON.stringify(doc.body)))
