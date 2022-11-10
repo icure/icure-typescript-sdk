f506b0968b52086cd6555ffae7820103bda7d394
Clément VANDENDAELEN <cv@icure.com>
2022-11-10 14:12:17 +0100

    Fixed code smells

--- icc-x-api/storage/LocalStorageImpl.ts
+++ icc-x-api/storage/LocalStorageImpl.ts
@@ -4,7 +4,7 @@ export class LocalStorageImpl implements StorageFacade<string> {
   async getItem(key: string): Promise<string | undefined> {
     if (typeof Storage === 'undefined') {
       console.error('Your browser does not support HTML5 Browser Local Storage !')
-      throw 'Your browser does not support HTML5 Browser Local Storage !'
+      throw new Error('Your browser does not support HTML5 Browser Local Storage !')
     }
     return localStorage.getItem(key) ?? undefined
   }
@@ -12,7 +12,7 @@ export class LocalStorageImpl implements StorageFacade<string> {
   async removeItem(key: string): Promise<void> {
     if (typeof Storage === 'undefined') {
       console.error('Your browser does not support HTML5 Browser Local Storage !')
-      throw 'Your browser does not support HTML5 Browser Local Storage !'
+      throw new Error('Your browser does not support HTML5 Browser Local Storage !')
     }
     return localStorage.removeItem(key)
   }
@@ -20,7 +20,7 @@ export class LocalStorageImpl implements StorageFacade<string> {
   async setItem(key: string, valueToStore: string): Promise<void> {
     if (typeof Storage === 'undefined') {
       console.error('Your browser does not support HTML5 Browser Local Storage !')
-      throw 'Your browser does not support HTML5 Browser Local Storage !'
+      throw new Error('Your browser does not support HTML5 Browser Local Storage !')
     }
     return localStorage.setItem(key, valueToStore)
   }
