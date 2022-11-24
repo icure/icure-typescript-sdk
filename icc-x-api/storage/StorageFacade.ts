01f69a52c44a2ea4444e764c2ec9a1851f1211fd
Clément VANDENDAELEN <cv@icure.com>
2022-11-24 15:18:59 +0100

    Fixed typo in doc

--- icc-x-api/storage/StorageFacade.ts
+++ icc-x-api/storage/StorageFacade.ts
@@ -2,7 +2,7 @@ export interface StorageFacade<T> {
   /**
    * Returns the value of the provided key from the storage
    * @param key Key of the value to retrieve
-   * @return The value associated to the provided key or null if not found.
+   * @return The value associated to the provided key or undefined if not found.
    */
   getItem(key: string): Promise<T | undefined>
 
