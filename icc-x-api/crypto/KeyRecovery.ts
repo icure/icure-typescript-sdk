19635a906b2d6fda98dae0cf23fc663875a04a0e
Luca Tremamunno <luca@icure.com>
2022-12-14 17:41:23 +0100

    Add shamir key recovery

---
+++ icc-x-api/crypto/KeyRecovery.ts
@@ -0,0 +1,246 @@
+import { DataOwnerWithType, IccDataOwnerXApi } from '../icc-data-owner-x-api'
+import { KeyPair } from './RSA'
+import { CryptoPrimitives } from './CryptoPrimitives'
+import { BaseExchangeKeysManager } from './BaseExchangeKeysManager'
+import { hex2ua, ua2hex } from '../utils'
+import { fingerprintToPublicKeysMapOf } from './utils'
+
+/**
+ * @internal this class is intended only for internal use and may be changed without notice.
+ *
+ */
+export class KeyRecovery {
+  private readonly primitives: CryptoPrimitives
+  private readonly baseExchangeKeysManager: BaseExchangeKeysManager
+  private readonly dataOwnerApi: IccDataOwnerXApi
+
+  constructor(primitives: CryptoPrimitives, baseExchangeKeysManager: BaseExchangeKeysManager, dataOwnerApi: IccDataOwnerXApi) {
+    this.primitives = primitives
+    this.baseExchangeKeysManager = baseExchangeKeysManager
+    this.dataOwnerApi = dataOwnerApi
+  }
+
+  /**
+   * Attempt to recover private keys of a data owner using all available key recovery methods and all available private keys. The method will
+   * automatically try to use newly recovered key pairs to recover additional key pairs.
+   * @param dataOwner a data owner.
+   * @param knownKeys keys available for the data owner.
+   * @return new key pairs (exclude already known pairs) which could be recovered using the known keys.
+   */
+  async recoverKeys(
+    dataOwner: DataOwnerWithType,
+    knownKeys: { [pubKeyFp: string]: KeyPair<CryptoKey> }
+  ): Promise<{ [pubKeyFp: string]: KeyPair<CryptoKey> }> {
+    const selfPublicKeys = Array.from(this.dataOwnerApi.getHexPublicKeysOf(dataOwner))
+    const knownKeysFpSet = new Set(Object.keys(knownKeys))
+    const missingKeysFpSet = new Set(selfPublicKeys.map((x) => x.slice(-32)).filter((x) => !knownKeysFpSet.has(x)))
+
+    const recoveryFunctions = [this.recoverFromTransferKeys, this.recoverFromShamirSplitKeys]
+    let allPrivateKeys: { [pubKeyFingerprint: string]: KeyPair<CryptoKey> } = { ...knownKeys }
+    let foundNewPrivateKeys = true
+
+    while (missingKeysFpSet.size > 0 && foundNewPrivateKeys) {
+      foundNewPrivateKeys = false
+      for (const recover of recoveryFunctions) {
+        const recovered = await recover(dataOwner, allPrivateKeys, missingKeysFpSet)
+        if (Object.keys(recovered).length > 0) {
+          foundNewPrivateKeys = true
+          Object.keys(recovered).forEach((fp) => missingKeysFpSet.delete(fp))
+          allPrivateKeys = { ...allPrivateKeys, ...recovered }
+        }
+      }
+    }
+
+    return Object.fromEntries(Object.entries(allPrivateKeys).filter(([keyFp]) => !knownKeysFpSet.has(keyFp)))
+  }
+
+  /*TODO?
+   * Ask access back suggestion: if by getting access back to an exchange key with another data owner I may recover additional keys we could suggest
+   * to ask for access back to that data owner.
+   */
+
+  private async recoverFromShamirSplitKeys(
+    dataOwner: DataOwnerWithType,
+    allKeys: { [pubKeyFingerprint: string]: KeyPair<CryptoKey> },
+    missingKeysFp: Set<string>
+  ): Promise<{ [pubKeyFingerprint: string]: KeyPair<CryptoKey> }> {
+    if (
+      dataOwner.dataOwner.publicKey &&
+      dataOwner.dataOwner.privateKeyShamirPartitions &&
+      Object.keys(dataOwner.dataOwner.privateKeyShamirPartitions).length > 0
+    ) {
+      const selfId = dataOwner.dataOwner.id!
+      const shamirSplits: { [keyPairFp: string]: { [delegateId: string]: string } } = {
+        [dataOwner.dataOwner.publicKey!.slice(-32)]: dataOwner.dataOwner.privateKeyShamirPartitions!,
+      }
+      const delegatesOfSplits = Array.from(
+        new Set(
+          Object.entries(shamirSplits).flatMap(([splitKeyFp, splitKeyData]) => (missingKeysFp.has(splitKeyFp) ? Object.keys(splitKeyData) : []))
+        )
+      )
+      const exchangeKeys = await delegatesOfSplits.reduce(async (acc, delegate) => {
+        const awaitedAcc = await acc
+        const currExchangeKeys = await this.baseExchangeKeysManager.getEncryptedExchangeKeysFor(selfId, delegate)
+        const decryptedExchangeKeys = (await this.baseExchangeKeysManager.tryDecryptExchangeKeys(currExchangeKeys, allKeys)).successfulDecryptions
+        return { ...awaitedAcc, [delegate]: decryptedExchangeKeys }
+      }, Promise.resolve({} as { [delegateId: string]: CryptoKey[] }))
+      return this.recoverWithSplits(dataOwner, shamirSplits, exchangeKeys)
+    } else return {}
+  }
+
+  private async recoverWithSplits(
+    dataOwner: DataOwnerWithType,
+    splits: { [keyPairFp: string]: { [delegateId: string]: string } },
+    exchangeKeys: { [delegateId: string]: CryptoKey[] }
+  ): Promise<{ [pubKeyFingerprint: string]: KeyPair<CryptoKey> }> {
+    const res: { [pubKeyFingerprint: string]: KeyPair<CryptoKey> } = {}
+    const keysByFp = fingerprintToPublicKeysMapOf(dataOwner)
+    for (const [fp, split] of Object.entries(splits)) {
+      const recovered = await this.tryRecoverSplitPrivate(split, exchangeKeys)
+      if (recovered) {
+        const pub = keysByFp[fp]
+        if (!pub) {
+          console.warn(`Missing public key for fingerprint ${fp} of recovered shamir key.`)
+        } else
+          try {
+            res[pub] = {
+              privateKey: recovered,
+              publicKey: await this.primitives.RSA.importKey('spki', hex2ua(pub), ['encrypt']),
+            }
+          } catch (e) {
+            console.warn(`Failed to import public key ${pub}`, e)
+          }
+      }
+    }
+    return res
+  }
+
+  private async tryRecoverSplitPrivate(
+    split: { [delegateId: string]: string },
+    exchangeKeys: { [delegateId: string]: CryptoKey[] }
+  ): Promise<CryptoKey | undefined> {
+    const splitsCount = Object.keys(split).length
+    if (splitsCount === 1) {
+      // Not sure about the key nor if the key is accessible
+      const [delegate, encryptedKey] = Object.entries(split)[0]
+      for (const exchangeKey of exchangeKeys[delegate] ?? []) {
+        try {
+          const decrypted = await this.primitives.AES.decrypt(exchangeKey, hex2ua(encryptedKey))
+          const importedKey = await this.primitives.RSA.importKey('pkcs8', decrypted, ['decrypt'])
+          if (importedKey) return importedKey
+        } catch (e) {}
+      }
+      return undefined
+    } else {
+      const decryptedSplits: string[] = []
+      for (const delegateAndEncryptedSplit of Object.entries(split)) {
+        const decrypted = await this.tryDecryptSplitPiece(delegateAndEncryptedSplit, exchangeKeys, splitsCount)
+        if (decrypted) decryptedSplits.push(ua2hex(decrypted))
+      }
+      return await this.primitives.RSA.importKey('pkcs8', hex2ua(this.primitives.shamir.combine(decryptedSplits)), ['decrypt'])
+    }
+  }
+
+  private async tryDecryptSplitPiece(
+    delegateAndEncryptedSplit: [string, string],
+    exchangeKeys: { [delegateId: string]: CryptoKey[] },
+    splitsCount: number
+  ): Promise<ArrayBuffer | undefined> {
+    const [delegate, encryptedPiece] = delegateAndEncryptedSplit
+    for (const exchangeKey of exchangeKeys[delegate] ?? []) {
+      try {
+        const decrypted = await this.primitives.AES.decrypt(exchangeKey, hex2ua(encryptedPiece))
+        if (this.validateDecryptedSplit(decrypted, splitsCount)) return decrypted
+      } catch (e) {}
+    }
+    return undefined
+  }
+
+  private validateDecryptedSplit(decryptedSplit: ArrayBuffer, splitsCount: number): boolean {
+    // Shamir split has size 256 * x + 3, and starts with 8 followed by the index of the split in hexadecimal
+    const decryptedHex = ua2hex(decryptedSplit)
+    if (decryptedHex.length % 256 !== 3 || decryptedHex[0] !== '8') return false
+    try {
+      const splitIndex = parseInt(decryptedHex.slice(1, 3))
+      return splitIndex > 0 && splitIndex <= splitsCount
+    } catch (e) {
+      return false
+    }
+  }
+
+  private async recoverFromTransferKeys(
+    dataOwner: DataOwnerWithType,
+    allKeys: { [pubKeyFingerprint: string]: KeyPair<CryptoKey> },
+    missingKeysFp: Set<string>
+  ): Promise<{ [pubKeyFingerprint: string]: KeyPair<CryptoKey> }> {
+    const publicKeyFingerprintToPublicKey = fingerprintToPublicKeysMapOf(dataOwner.dataOwner)
+    const missingKeysTransferData: { [recoverableKeyPubFp: string]: { publicKey: string; encryptedPrivateKey: Set<string> } } = {}
+    Object.values(dataOwner.dataOwner.transferKeys ?? {}).forEach((transferKeysByEncryptor) => {
+      Object.entries(transferKeysByEncryptor).forEach(([transferToPublicKey, transferPrivateKeyEncrypted]) => {
+        const transferToPublicKeyFp = transferToPublicKey.slice(-32) // We are not sure if transfer public key will be a fp or not
+        if (missingKeysFp.has(transferToPublicKeyFp)) {
+          const existingEntryValue = missingKeysTransferData[transferToPublicKeyFp]
+          if (existingEntryValue) {
+            existingEntryValue.encryptedPrivateKey.add(transferPrivateKeyEncrypted)
+          } else {
+            const fullPublicKey = publicKeyFingerprintToPublicKey[transferToPublicKeyFp]
+            if (fullPublicKey) {
+              missingKeysTransferData[transferToPublicKeyFp] = {
+                publicKey: fullPublicKey,
+                encryptedPrivateKey: new Set([transferPrivateKeyEncrypted]),
+              }
+            } else {
+              console.warn('Invalid data owner: there is a transfer key for an unknown public key')
+            }
+          }
+        }
+      })
+    })
+
+    const { successfulDecryptions: availableExchangeKeys } = await this.baseExchangeKeysManager.tryDecryptExchangeKeys(
+      await this.baseExchangeKeysManager.getEncryptedExchangeKeysFor(dataOwner.dataOwner.id!, dataOwner.dataOwner.id!),
+      allKeys
+    )
+
+    return Object.entries(missingKeysTransferData).reduce(async (acc, [recoverableKeyPubFp, transferData]) => {
+      const awaitedAcc = await acc
+      const decryptedTransferData = await this.tryDecryptTransferData(transferData, availableExchangeKeys)
+      if (decryptedTransferData != undefined) {
+        return { ...awaitedAcc, [recoverableKeyPubFp]: decryptedTransferData }
+      } else return awaitedAcc
+    }, Promise.resolve({} as { [pubKeyFingerprint: string]: KeyPair<CryptoKey> }))
+  }
+
+  private async tryDecryptTransferData(
+    transferData: { publicKey: string; encryptedPrivateKey: Set<string> },
+    availableExchangeKeys: CryptoKey[]
+  ): Promise<KeyPair<CryptoKey> | undefined> {
+    for (const encryptedTransferKey of transferData.encryptedPrivateKey) {
+      const decryptedTransferKey = await this.tryDecryptTransferKey(encryptedTransferKey, availableExchangeKeys)
+      if (decryptedTransferKey != undefined)
+        return {
+          privateKey: decryptedTransferKey,
+          publicKey: await this.primitives.RSA.importKey('spki', hex2ua(transferData.publicKey), ['encrypt']),
+        }
+    }
+    return undefined
+  }
+
+  // attempt to decrypt a transfer key in pkcs8 using any of the provided exchange keys
+  private async tryDecryptTransferKey(
+    encryptedTransferKey: string, // in hex format
+    exchangeKeys: CryptoKey[]
+  ): Promise<CryptoKey | undefined> {
+    const encryptedKeyBytes = hex2ua(encryptedTransferKey)
+    for (const exchangeKey of exchangeKeys) {
+      try {
+        const decryptedKeyData = await this.primitives.AES.decrypt(exchangeKey, encryptedKeyBytes)
+        const importedPrivateKey = await this.primitives.RSA.importPrivateKey('pkcs8', decryptedKeyData)
+        if (importedPrivateKey != undefined) return importedPrivateKey
+      } catch (e) {
+        /* failure is a valid possibility: we don't know the correct key to use */
+      }
+    }
+    return undefined
+  }
+}
--- icc-x-api/crypto/TransferKeysManager.ts
+++ icc-x-api/crypto/TransferKeysManager.ts
@@ -1,8 +1,7 @@
-import { AESUtils } from './AES'
-import { KeyPair, RSAUtils } from './RSA'
+import { KeyPair } from './RSA'
 import { BaseExchangeKeysManager } from './BaseExchangeKeysManager'
 import { DataOwnerWithType, IccDataOwnerXApi } from '../icc-data-owner-x-api'
-import { hex2ua, ua2hex } from '../utils'
+import { ua2hex } from '../utils'
 import { KeyManager } from './KeyManager'
 import { reachSetsAcyclic, StronglyConnectedGraph } from '../utils/graph-utils'
 import { fingerprintToPublicKeysMapOf, loadPublicKeys, transferKeysFpGraphOf } from './utils'
@@ -10,64 +9,35 @@ import { CryptoPrimitives } from './CryptoPrimitives'
 
 /**
  * @internal this class is intended only for internal use and may be changed without notice.
- * Allows to manage transfer keys.
+ * Allows to create new transfer keys.
  */
 export class TransferKeysManager {
   private readonly primitives: CryptoPrimitives
   private readonly baseExchangeKeysManager: BaseExchangeKeysManager
   private readonly dataOwnerApi: IccDataOwnerXApi
+  private readonly keyManager: KeyManager
 
-  constructor(primitives: CryptoPrimitives, baseExchangeKeysManager: BaseExchangeKeysManager, dataOwnerApi: IccDataOwnerXApi) {
+  constructor(
+    primitives: CryptoPrimitives,
+    baseExchangeKeysManager: BaseExchangeKeysManager,
+    dataOwnerApi: IccDataOwnerXApi,
+    keyManager: KeyManager
+  ) {
     this.primitives = primitives
     this.baseExchangeKeysManager = baseExchangeKeysManager
     this.dataOwnerApi = dataOwnerApi
+    this.keyManager = keyManager
   }
 
   /**
-   * Analyses the transfer keys graph for the current data owner and suggests potential keys which could get better access to data by adding new
-   * transfer keys.
-   * This method is thought to be used in conjunction with {@link updateTransferKeys} for the creation of new transfer keys: after the user has
-   * verified that he owns the public keys returned by this method (or only some of them) you can call {@link updateTransferKeys } with the verified
-   * public keys to create the new transfer keys.
-   *
-   * @param self the current data owner
-   * @param keyManager key manager for the current data owner.
-   * @return public keys of key pairs which would benefit from new transfer keys, in hex spki format.
-   */
-  async getNewTransferKeysSuggestions(self: DataOwnerWithType, keyManager: KeyManager): Promise<Set<string>> {
-    const graph = transferKeysFpGraphOf(self)
-    const candidatesFp = this.transferKeysCandidatesFp((await this.transferTargetDeviceKey(keyManager)).fingerprint, graph)
-    const fpToFullMap = fingerprintToPublicKeysMapOf(self)
-    const deviceKeysFp = new Set((await keyManager.getSelfVerifiedKeys()).map((x) => x.fingerprint))
-    return new Set(
-      candidatesFp
-        .flatMap((candidate) => graph.acyclicLabelToGroup[candidate])
-        // Exclude keys from the device, in general they should not be candidates, but if it happens we can automatically consider them as verified.
-        .filter((fp) => !deviceKeysFp.has(fp))
-        .map((fp) => fpToFullMap[fp])
-    )
-  }
-
-  /**
-   * Create new transfer keys for the current data owner. This can be used to improve data accessibility from other devices or to implement a data
-   * recovery function.
-   *
-   * ## Important
-   *
-   * The creation of new transfer keys should be done only after approval by the end-user. This feature effectively allows to share the private keys
-   * currently available on this device with anyone that has access to a specific private key. Usually this would be another private key of the user
-   * in a different device, but in case iCure got compromised this could be the key of an attacker.
-   *
-   * For this reason before creating transfer keys you should always let the user verify that the public keys which would be used for the encryption
-   * of the new transfer keys are actually owned by the user themselves.
-   *
-   * @param self the current data owner
-   * @param keyManager key manager for the current data owner.
-   * @param verifiedPublicKeys public keys in hex spki format that the current data owner has confirmed he owns.
+   * Analyses the transfer keys graph and creates new transfer keys which allow to improve data accessibility from other devices.
+   * For security reasons transfer keys will only be created between keys verified by the user, but this will be done ignoring any existing edges from
+   * unverified keys to verified keys.
+   * @param self the current data owner.
    * @return the updated data owner.
    */
-  async updateTransferKeys(self: DataOwnerWithType, keyManager: KeyManager, verifiedPublicKeys: string[]): Promise<void> {
-    const newEdges = await this.getNewVerifiedTransferKeysEdges(self, keyManager, verifiedPublicKeys)
+  async updateTransferKeys(self: DataOwnerWithType): Promise<void> {
+    const newEdges = await this.getNewVerifiedTransferKeysEdges(self)
     if (!newEdges) return
     const selfId = await this.dataOwnerApi.getCurrentDataOwnerId()
     const fpToPublicKey = fingerprintToPublicKeysMapOf(self)
@@ -98,102 +68,6 @@ export class TransferKeysManager {
     })
   }
 
-  /**
-   * Load all key pairs for the current data owner which can be obtained from the currently known keys and the transfer keys.
-   * @param self the current data owner
-   * @param knownKeys key pairs already loaded for the current user.
-   * @return new key pairs (exclude already known pairs) which could be loaded from the transfer keys using the known keys.
-   */
-  async loadSelfKeysFromTransfer(
-    self: DataOwnerWithType,
-    knownKeys: { [pubKeyFingerprint: string]: KeyPair<CryptoKey> }
-  ): Promise<{ [pubKeyFingerprint: string]: KeyPair<CryptoKey> }> {
-    const selfPublicKeys = Array.from(this.dataOwnerApi.getHexPublicKeysOf(self))
-    const loadedStoredKeysFingerprintsSet = new Set(Object.keys(knownKeys))
-    // The same private key may be encrypted using different exchange keys.
-    const missingKeysTransferData: { [recoverableKeyPubFp: string]: { publicKey: string; encryptedPrivateKey: Set<string> } } = {}
-    Object.values(self.dataOwner.transferKeys ?? {}).forEach((transferKeysByEncryptor) => {
-      Object.entries(transferKeysByEncryptor).forEach(([transferPublicKey, transferPrivateKeyEncrypted]) => {
-        const transferPublicKeyFp = transferPublicKey.slice(-32) // We are not sure if transfer public key will be a fp or not
-        if (!loadedStoredKeysFingerprintsSet.has(transferPublicKeyFp)) {
-          const existingEntryValue = missingKeysTransferData[transferPublicKeyFp]
-          if (existingEntryValue != undefined) {
-            existingEntryValue.encryptedPrivateKey.add(transferPrivateKeyEncrypted)
-          } else {
-            const fullPublicKey = selfPublicKeys.find((x) => x.slice(-32) == transferPublicKeyFp)
-            if (fullPublicKey != undefined) {
-              missingKeysTransferData[transferPublicKeyFp] = {
-                publicKey: fullPublicKey,
-                encryptedPrivateKey: new Set([transferPrivateKeyEncrypted]),
-              }
-            } else {
-              console.warn('Invalid data owner: there is a transfer key for an unknown public key')
-            }
-          }
-        }
-      })
-    })
-
-    // Load initially available exchange keys
-    let { successfulDecryptions: availableExchangeKeys, failedDecryptions: encryptedExchangeKeys } =
-      await this.baseExchangeKeysManager.tryDecryptExchangeKeys(
-        await this.baseExchangeKeysManager.getEncryptedExchangeKeysFor(self.dataOwner.id!, self.dataOwner.id!),
-        knownKeys
-      )
-
-    let result: { [pubKeyFingerprint: string]: KeyPair<CryptoKey> } = {}
-
-    while (availableExchangeKeys.length > 0 && Object.keys(missingKeysTransferData).length > 0) {
-      const newRecoveredKeys: { [pubKeyFingerprint: string]: KeyPair<CryptoKey> } = {}
-      for (const [recoverableKeyPubFp, transferData] of Object.entries(missingKeysTransferData)) {
-        const decryptedTransferData = await this.tryDecryptTransferData(transferData, availableExchangeKeys)
-        if (decryptedTransferData != undefined) {
-          newRecoveredKeys[recoverableKeyPubFp] = decryptedTransferData
-          delete missingKeysTransferData[recoverableKeyPubFp]
-        }
-      }
-      const newExchangeKeysDecryption = await this.baseExchangeKeysManager.tryDecryptExchangeKeys(encryptedExchangeKeys, newRecoveredKeys)
-      availableExchangeKeys = newExchangeKeysDecryption.successfulDecryptions
-      encryptedExchangeKeys = newExchangeKeysDecryption.failedDecryptions
-      result = { ...result, ...newRecoveredKeys }
-    }
-
-    return result
-  }
-
-  private async tryDecryptTransferData(
-    transferData: { publicKey: string; encryptedPrivateKey: Set<string> },
-    availableExchangeKeys: CryptoKey[]
-  ): Promise<KeyPair<CryptoKey> | undefined> {
-    for (const encryptedTransferKey of transferData.encryptedPrivateKey) {
-      const decryptedTransferKey = await this.tryDecryptTransferKey(encryptedTransferKey, availableExchangeKeys)
-      if (decryptedTransferKey != undefined)
-        return {
-          privateKey: decryptedTransferKey,
-          publicKey: await this.primitives.RSA.importKey('spki', hex2ua(transferData.publicKey), ['encrypt']),
-        }
-    }
-    return undefined
-  }
-
-  // attempt to decrypt a transfer key in pkcs8 using any of the provided exchange keys
-  private async tryDecryptTransferKey(
-    encryptedTransferKey: string, // in hex format
-    exchangeKeys: CryptoKey[]
-  ): Promise<CryptoKey | undefined> {
-    const encryptedKeyBytes = hex2ua(encryptedTransferKey)
-    for (const exchangeKey of exchangeKeys) {
-      try {
-        const decryptedKeyData = await this.primitives.AES.decrypt(exchangeKey, encryptedKeyBytes)
-        const importedPrivateKey = await this.primitives.RSA.importPrivateKey('pkcs8', decryptedKeyData)
-        if (importedPrivateKey != undefined) return importedPrivateKey
-      } catch (e) {
-        /* failure is a valid possibility: we don't know the correct key to use */
-      }
-    }
-    return undefined
-  }
-
   // encrypts a transfer key in pkcs8 format using an exchange key, returns the hex representation
   private async encryptTransferKey(transferKey: KeyPair<CryptoKey>, exchangeKey: CryptoKey): Promise<string> {
     const exportedKey = await this.primitives.RSA.exportKey(transferKey.privateKey, 'pkcs8')
@@ -208,16 +82,12 @@ export class TransferKeysManager {
       .map((x) => x[0])
   }
 
-  private async transferTargetDeviceKey(keyManager: KeyManager): Promise<{ fingerprint: string; pair: KeyPair<CryptoKey> }> {
-    return (await keyManager.getSelfVerifiedKeys())[0]
+  private async transferTargetVerifiedKey(keyManager: KeyManager): Promise<{ fingerprint: string; pair: KeyPair<CryptoKey> }> {
+    return keyManager.getSelfVerifiedKeys()[0]
   }
 
   // Decides the best edges considering the verified public keys.
-  private async getNewVerifiedTransferKeysEdges(
-    self: DataOwnerWithType,
-    keyManager: KeyManager,
-    verifiedPublicKeys: string[]
-  ): Promise<
+  private async getNewVerifiedTransferKeysEdges(self: DataOwnerWithType): Promise<
     | undefined
     | {
         sources: string[]
@@ -225,24 +95,27 @@ export class TransferKeysManager {
         targetFp: string
       }
   > {
-    if (verifiedPublicKeys.length == 0) return undefined
-    const verifiedKeysFpSet = new Set(verifiedPublicKeys.map((x) => x.slice(-32)))
-    // All keys of this device should be considered as verified, and if for some reasons they are not connected we can connect them.
-    ;(await keyManager.getSelfVerifiedKeys()).forEach((dk) => verifiedKeysFpSet.add(dk.fingerprint))
+    const verifiedKeysFpSet = new Set(this.keyManager.getSelfVerifiedKeys().map((x) => x.fingerprint))
+    if (verifiedKeysFpSet.size == 0) return undefined
     const graph = transferKeysFpGraphOf(self)
-    const { fingerprint: targetKeyFp, pair: targetKey } = await this.transferTargetDeviceKey(keyManager)
+    // 1. Choose a key available in this device which should be reachable from all other verified keys
+    const { fingerprint: targetKeyFp, pair: targetKey } = await this.transferTargetVerifiedKey(this.keyManager)
+    // 2. Find groups which can't reach the existing target keys
     const candidatesFp = this.transferKeysCandidatesFp(targetKeyFp, graph)
+    // 3. Keep only groups which contain at least a verified candidate
     const verifiedGroupCandidates = candidatesFp.filter((candidate) =>
       graph.acyclicLabelToGroup[candidate].some((candidateGroupMember) => verifiedKeysFpSet.has(candidateGroupMember))
     )
     const verifiedCandidatesSet = new Set(verifiedGroupCandidates)
+    if (verifiedCandidatesSet.size == 0) return undefined
+    // 4. Drop all candidates which can already reach another candidate group: it is sufficient to create a transfer key from that group.
     const reachSets = reachSetsAcyclic(graph.acyclicGraph)
-    // Drop all candidates which can reach another candidate group: it is sufficient to create a transfer key from that group.
     const optimizedCandidates = verifiedGroupCandidates.filter((candidate) =>
       Array.from(reachSets[candidate]).every((reachableNode) => !verifiedCandidatesSet.has(reachableNode))
     )
     if (optimizedCandidates.length == 0) throw new Error('Check failed: at least one candidate should survive optimization')
-    // Transfer keys could also be faked: to make sure we are not giving access to something wrong we remap the candidates to a verified public keys
+    // 5. Transfer keys could also be faked: to make sure we are not giving access to unauthorised people we remap the candidates to a verified public
+    // keys
     const verifiedOptimizedCandidates = optimizedCandidates.map((candidate) => {
       const res = graph.acyclicLabelToGroup[candidate].find((groupMemberFp) => verifiedKeysFpSet.has(groupMemberFp))
       if (!res) throw new Error('Check failed: optimized candidates groups should have at least a verified member')
