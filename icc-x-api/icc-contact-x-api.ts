9fbb6a1adb5c338af82df24ebf64f173aaea19dc
LAPSAM6\sjocq <sam.jocque@medispring.be>
2023-03-01 12:23:38 +0100

    MS-8722: Use POST method for find[...]ByHCPartyPatientForeignKeys -> add parameter to allow choice between get and post versions

--- icc-x-api/icc-contact-x-api.ts
+++ icc-x-api/icc-contact-x-api.ts
@@ -163,8 +163,9 @@ export class IccContactXApi extends IccContactApi {
    *
    * @param hcpartyId
    * @param patient (Promise)
+   * @param usingPost
    */
-  findBy(hcpartyId: string, patient: models.Patient) {
+  findBy(hcpartyId: string, patient: models.Patient, usingPost: boolean = false) {
     return this.crypto.extractSFKsHierarchyFromDelegations(patient, hcpartyId).then((secretForeignKeys) =>
       secretForeignKeys && secretForeignKeys.length > 0
         ? Promise.all(
@@ -178,43 +179,10 @@ export class IccContactXApi extends IccContactApi {
                 ])
               }, [] as Array<{ hcpartyId: string; extractedKeys: Array<string> }>)
               .filter((l) => l.extractedKeys.length > 0)
-              .map(({ hcpartyId, extractedKeys }) => this.findByHCPartyPatientSecretFKeys(hcpartyId, _.uniq(extractedKeys).join(',')))
+              .map(({ hcpartyId, extractedKeys }) => usingPost ?
+                this.findByHCPartyPatientSecretFKeysArray(hcpartyId, _.uniq(extractedKeys)) :
+                this.findByHCPartyPatientSecretFKeys(hcpartyId, _.uniq(extractedKeys).join(',')))
           ).then((results) => _.uniqBy(_.flatMap(results), (x) => x.id))
-        : Promise.resolve([])
-    )
-  }
-
-  /**
-   * 1. Check whether there is a delegation with 'hcpartyId' or not.
-   * 2. 'fetchHcParty[hcpartyId][1]': is encrypted AES exchange key by RSA public key of him.
-   * 3. Obtain the AES exchange key, by decrypting the previous step value with hcparty private key
-   *      3.1.  KeyPair should be fetch from cache (in jwk)
-   *      3.2.  if it doesn't exist in the cache, it has to be loaded from Browser Local store, and then import it to WebCrypto
-   * 4. Obtain the array of delegations which are delegated to his ID (hcpartyId) in this patient
-   * 5. Decrypt and collect all keys (secretForeignKeys) within delegations of previous step (with obtained AES key of step 4)
-   * 6. Do the REST call to get all contacts with (allSecretForeignKeysDelimitedByComa, hcpartyId)
-   *
-   * After these painful steps, you have the contacts of the patient.
-   *
-   * @param hcpartyId
-   * @param patient (Promise)
-   */
-  findByUsingPost(hcpartyId: string, patient: models.Patient) {
-    return this.crypto.extractSFKsHierarchyFromDelegations(patient, hcpartyId).then((secretForeignKeys) =>
-      secretForeignKeys && secretForeignKeys.length > 0
-        ? Promise.all(
-          secretForeignKeys
-            .reduce((acc, level) => {
-              return acc.concat([
-                {
-                  hcpartyId: level.hcpartyId,
-                  extractedKeys: level.extractedKeys.filter((key) => !acc.some((previousLevel) => previousLevel.extractedKeys.includes(key))),
-                },
-              ])
-            }, [] as Array<{ hcpartyId: string; extractedKeys: Array<string> }>)
-            .filter((l) => l.extractedKeys.length > 0)
-            .map(({ hcpartyId, extractedKeys }) => this.findByHCPartyPatientSecretFKeysArray(hcpartyId, _.uniq(extractedKeys)))
-        ).then((results) => _.uniqBy(_.flatMap(results), (x) => x.id))
         : Promise.resolve([])
     )
   }
