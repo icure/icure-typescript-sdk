0552b8badd9145c1b92b9a72efebcbea483db22c
LAPSAM6\sjocq <sam.jocque@medispring.be>
2023-03-16 09:53:54 +0100

    MS-8722: test for icc-accesslog-x-api findBy Using Post

--- test/icc-x-api/icc-accesslog-x-api.ts
+++ test/icc-x-api/icc-accesslog-x-api.ts
@@ -1,9 +1,14 @@
 import 'isomorphic-fetch'
-import { getEnvironmentInitializer, getEnvVariables, hcp1Username, setLocalStorage, TestVars } from '../utils/test_utils'
+import {getEnvironmentInitializer, getEnvVariables, hcp1Username, setLocalStorage, TestUtils, TestVars} from '../utils/test_utils'
 import { before } from 'mocha'
-import { IccUserXApi } from '../../icc-x-api'
+import {Api, IccAccesslogXApi, IccPatientXApi, IccUserXApi} from '../../icc-x-api'
 import { BasicAuthenticationProvider } from '../../icc-x-api/auth/AuthenticationProvider'
 import { IccAccesslogApi } from '../../icc-api'
+import {Patient} from "../../icc-api/model/Patient"
+import {User} from "../../icc-api/model/User"
+import {randomUUID} from "crypto"
+import {crypto} from "../../node-compat"
+import initKey = TestUtils.initKey
 
 setLocalStorage(fetch)
 let env: TestVars
@@ -14,6 +19,21 @@ describe('icc-x-accesslog-api Tests', () => {
     const initializer = await getEnvironmentInitializer()
     env = await initializer.execute(getEnvVariables())
   })
+
+  async function createPatient(patientApiForHcp: IccPatientXApi, hcpUser: User) {
+    return patientApiForHcp.createPatientWithUser(
+      hcpUser,
+      await patientApiForHcp.newInstance(
+        hcpUser,
+        new Patient({
+          id: randomUUID(),
+          firstName: 'John',
+          lastName: 'Snow',
+          note: 'Winter is coming',
+        })
+      )
+    )
+  }
 
   it('Test', async () => {
     // Given
@@ -28,5 +48,29 @@ describe('icc-x-accesslog-api Tests', () => {
     const currentUser = await userApi.getCurrentUser()
 
     await accessLogApi.findByUserAfterDate(currentUser.id!)
+  })
+
+  it('Test usingPost', async () => {
+    // Given
+    const {
+      userApi: userApiForHcp,
+      dataOwnerApi: dataOwnerApiForHcp,
+      patientApi: patientApiForHcp,
+      cryptoApi: cryptoApiForHcp,
+      dataOwnerApi: dateOwnerApiForHcp,
+    } = await Api(env!.iCureUrl, env!.dataOwnerDetails[hcp1Username].user, env!.dataOwnerDetails[hcp1Username].password, crypto)
+    const hcpUser = await userApiForHcp.getCurrentUser()
+    await initKey(dataOwnerApiForHcp, cryptoApiForHcp, hcpUser, env!.dataOwnerDetails[hcp1Username].privateKey)
+
+    const username = env.dataOwnerDetails[hcp1Username].user
+    const password = env.dataOwnerDetails[hcp1Username].password
+
+    const authProvider = new BasicAuthenticationProvider(username, password)
+
+    const accessLogXApi = new IccAccesslogXApi(env.iCureUrl, {}, cryptoApiForHcp, dateOwnerApiForHcp, authProvider, fetch)
+
+    const patient = (await createPatient(patientApiForHcp, hcpUser)) as Patient
+
+    await accessLogXApi.findBy(hcpUser.healthcarePartyId!, patient, true)
   })
 })
