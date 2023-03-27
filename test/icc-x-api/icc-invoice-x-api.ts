b63a48f26f62424e5e364f4dd4795022eafd56e5
LAPSAM6\sjocq <sam.jocque@medispring.be>
2023-03-27 14:04:04 +0200

    MS-8722: test for icc-invoice-x-api.ts fix + add asserts

--- test/icc-x-api/icc-invoice-x-api.ts
+++ test/icc-x-api/icc-invoice-x-api.ts
@@ -9,6 +9,8 @@ import {User} from "../../icc-api/model/User"
 import {randomUUID} from "crypto"
 import {crypto} from "../../node-compat"
 import initKey = TestUtils.initKey
+import {Invoice} from "../../icc-api/model/Invoice"
+import {assert} from "chai"
 
 setLocalStorage(fetch)
 let env: TestVars
@@ -66,36 +68,33 @@ describe('icc-calendar-item-x-api Tests', () => {
     const password = env.dataOwnerDetails[hcp1Username].password
 
     const authProvider = new BasicAuthenticationProvider(username, password)
-
     const invoiceXApi = new IccInvoiceXApi(env.iCureUrl, {}, cryptoApiForHcp, entityReferenceApiForHcp, dateOwnerApiForHcp, authProvider, fetch)
-
     const patient = (await createPatient(patientApiForHcp, hcpUser)) as Patient
 
-    await invoiceXApi.findBy(hcpUser.healthcarePartyId!, patient, false)
+    const invoice = new Invoice({
+      id: randomUUID(),
+      created: new Date().getTime(),
+      modified: new Date().getTime(),
+      date: +new Date(),
+      responsible: hcpUser.healthcarePartyId!,
+      author: hcpUser.id,
+      codes: [],
+      tags: [],
+      user: hcpUser.id,
+      patient: patient.id,
+    })
+
+    const invoiceToCreate = await invoiceXApi.newInstance(hcpUser, patient, invoice);
+    const createdInvoice = await invoiceXApi.createInvoice(invoiceToCreate);
+
+    const foundItems = await invoiceXApi.findBy(hcpUser.healthcarePartyId!, patient, false)
+    const foundItemsUsingPost = await invoiceXApi.findBy(hcpUser.healthcarePartyId!, patient, true)
+
+    assert(foundItems.length == 1, "Found items should be 1")
+    assert( foundItems[0].id == createdInvoice.id, "Found item should be the created invoice")
+
+    assert(foundItemsUsingPost.length == 1, "Found items using post should be 1")
+    assert( foundItemsUsingPost[0].id == createdInvoice.id, "Found item using post should be the created invoice")
   })
 
-  it('Test findBy usingPost', async () => {
-    // Given
-    const {
-      userApi: userApiForHcp,
-      dataOwnerApi: dataOwnerApiForHcp,
-      patientApi: patientApiForHcp,
-      cryptoApi: cryptoApiForHcp,
-      dataOwnerApi: dateOwnerApiForHcp,
-      entityReferenceApi: entityReferenceApiForHcp,
-    } = await Api(env!.iCureUrl, env!.dataOwnerDetails[hcp1Username].user, env!.dataOwnerDetails[hcp1Username].password, crypto)
-    const hcpUser = await userApiForHcp.getCurrentUser()
-    await initKey(dataOwnerApiForHcp, cryptoApiForHcp, hcpUser, env!.dataOwnerDetails[hcp1Username].privateKey)
-
-    const username = env.dataOwnerDetails[hcp1Username].user
-    const password = env.dataOwnerDetails[hcp1Username].password
-
-    const authProvider = new BasicAuthenticationProvider(username, password)
-
-    const invoiceXApi = new IccInvoiceXApi(env.iCureUrl, {}, cryptoApiForHcp, entityReferenceApiForHcp, dateOwnerApiForHcp, authProvider, fetch)
-
-    const patient = (await createPatient(patientApiForHcp, hcpUser)) as Patient
-
-    await invoiceXApi.findBy(hcpUser.healthcarePartyId!, patient, true)
-  })
 })
