9bbab421a38c215ae44945ab99e853df2bb4e1cc
Luca Tremamunno <luca@icure.com>
2023-05-10 14:13:06 +0200

    Add patient merge api

--- icc-api/api/IccPatientApi.ts
+++ icc-api/api/IccPatientApi.ts
@@ -819,4 +819,19 @@ export class IccPatientApi {
       .then((doc) => (doc.body as Array<JSON>).map((it) => new DocIdentifier(it)))
       .catch((err) => this.handleError(err))
   }
+
+  /**
+   * @internal use mergePatients from the extended api instead
+   */
+  baseMergePatients(fromId: string, expectedFromRev: string, updatedInto: Patient) {
+    let _body = updatedInto
+
+    const _url = this.host + `patient/mergeInto/${encodeURIComponent(String(updatedInto.id))}/from/${encodeURIComponent(String(fromId))}`
+    '?ts=' + new Date().getTime() + '&expectedFromRev=' + encodeURIComponent(String(expectedFromRev))
+    let headers = this.headers
+    headers = headers.filter((h) => h.header !== 'Content-Type').concat(new XHR.Header('Content-Type', 'application/json'))
+    return XHR.sendCommand('PUT', _url, headers, _body, this.fetchImpl, undefined, this.authenticationProvider.getAuthService())
+      .then((doc) => new Patient(doc.body as JSON))
+      .catch((err) => this.handleError(err))
+  }
 }
