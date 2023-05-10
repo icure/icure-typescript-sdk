d07f962c97014e22133155da7751dd2e24847013
Luca Tremamunno <luca@icure.com>
2023-05-10 16:57:24 +0200

    Add calendar item linking and patient merging tests

--- test/icc-x-api/icc-patient-x-api-test.ts
+++ test/icc-x-api/icc-patient-x-api-test.ts
@@ -3,9 +3,9 @@ import { before } from 'mocha'
 import 'isomorphic-fetch'
 
 import { Patient } from '../../icc-api/model/Patient'
-import { assert } from 'chai'
+import { assert, expect } from 'chai'
 import { randomUUID } from 'crypto'
-import { getEnvironmentInitializer, hcp1Username, setLocalStorage, TestUtils } from '../utils/test_utils'
+import { getEnvironmentInitializer, hcp1Username, hcp2Username, hcp3Username, setLocalStorage, TestUtils } from '../utils/test_utils'
 import initApi = TestUtils.initApi
 import { getEnvVariables, TestVars } from '@icure/test-setup/types'
 
@@ -52,5 +52,72 @@ describe('icc-x-patient-api Tests', () => {
     assert(readPatient.lastName == patientToCreate.lastName)
     assert(readPatient.delegations![hcpUser.healthcarePartyId!].length > 0)
     assert(readPatient.encryptionKeys![hcpUser.healthcarePartyId!].length > 0)
+  })
+
+  it('Merge patients should work as expected', async () => {
+    const api1 = await initApi(env!, hcp1Username)
+    const user1 = await api1.userApi.getCurrentUser()
+    const api2 = await initApi(env!, hcp2Username)
+    const user2 = await api2.userApi.getCurrentUser()
+    const api3 = await initApi(env!, hcp3Username)
+    const user3 = await api3.userApi.getCurrentUser()
+    const mergedFirstName = 'Gigio'
+    const mergedLastName = 'Bagigio'
+    const mergedAlias = 'Luigio'
+    const mergedNote = 'A secret note'
+    const patientFrom = await api1.patientApi.createPatientWithUser(
+      user1,
+      await api1.patientApi.newInstance(
+        user1,
+        {
+          firstName: mergedFirstName,
+          lastName: 'From',
+          note: 'A',
+        },
+        { additionalDelegates: { [user2.healthcarePartyId!]: 'WRITE' } }
+      )
+    )
+    const patientFromSecretIds = await api1.patientApi.decryptSecretIdsOf(patientFrom)
+    expect(patientFromSecretIds).to.have.lengthOf(1)
+    const patientInto = await api1.patientApi.createPatientWithUser(
+      user1,
+      await api1.patientApi.newInstance(
+        user1,
+        {
+          firstName: 'Into',
+          lastName: mergedLastName,
+          note: 'B',
+        },
+        { additionalDelegates: { [user3.healthcarePartyId!]: 'WRITE' } }
+      )
+    )
+    const patientIntoSecretIds = await api1.patientApi.decryptSecretIdsOf(patientInto)
+    expect(patientFromSecretIds).to.have.lengthOf(1)
+    expect(patientIntoSecretIds[0]).to.not.equal(patientFromSecretIds[0])
+    const mergedInto = {
+      ...patientInto,
+      firstName: mergedFirstName,
+      lastName: mergedLastName,
+      alias: mergedAlias,
+      note: mergedNote,
+    }
+    const mergedPatient = await api1.patientApi.mergePatients(patientFrom, mergedInto)
+    expect(mergedPatient.firstName).to.equal(mergedFirstName)
+    expect(mergedPatient.lastName).to.equal(mergedLastName)
+    expect(mergedPatient.alias).to.equal(mergedAlias)
+    expect(mergedPatient.note).to.equal(mergedNote)
+    expect(await api1.patientApi.decryptSecretIdsOf(mergedPatient)).to.have.members([...patientFromSecretIds, ...patientIntoSecretIds])
+    const retrievedByDelegateWithAccessToInto = await api3.patientApi.getPatientWithUser(user3, patientInto.id!)
+    expect(retrievedByDelegateWithAccessToInto.firstName).to.equal(mergedFirstName)
+    expect(retrievedByDelegateWithAccessToInto.lastName).to.equal(mergedLastName)
+    expect(retrievedByDelegateWithAccessToInto.alias).to.equal(mergedAlias)
+    expect(retrievedByDelegateWithAccessToInto.note).to.equal(mergedNote)
+    expect(await api3.patientApi.decryptSecretIdsOf(retrievedByDelegateWithAccessToInto)).to.have.members(patientIntoSecretIds)
+    const retrievedByDelegateWithAccessToFrom = await api2.patientApi.getPatientWithUser(user2, patientInto.id!)
+    expect(retrievedByDelegateWithAccessToFrom.firstName).to.equal(mergedFirstName)
+    expect(retrievedByDelegateWithAccessToFrom.lastName).to.equal(mergedLastName)
+    expect(retrievedByDelegateWithAccessToFrom.alias).to.equal(mergedAlias)
+    expect(retrievedByDelegateWithAccessToFrom.note).to.be.undefined // No access to new encryption key yet
+    expect(await api2.patientApi.decryptSecretIdsOf(retrievedByDelegateWithAccessToFrom)).to.have.members(patientFromSecretIds)
   })
 })
