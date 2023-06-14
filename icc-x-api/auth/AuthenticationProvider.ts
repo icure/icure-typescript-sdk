96e5734e7a9fac603c3f9756b4e8bbfd078bb6c7
Luca Tremamunno <luca@icure.com>
2023-06-14 16:33:20 +0200

    Add method to switch authentication providers to a different user group

--- icc-x-api/auth/AuthenticationProvider.ts
+++ icc-x-api/auth/AuthenticationProvider.ts
@@ -4,9 +4,27 @@ import { EnsembleAuthService } from './EnsembleAuthService'
 import { JwtAuthService } from './JwtAuthService'
 import { NoAuthService } from './NoAuthService'
 import { BasicAuthService } from './BasicAuthService'
+import { UserGroup } from '../../icc-api/model/UserGroup'
 
+/**
+ * @internal you should not instantiate implementations of this interface directly.
+ */
 export interface AuthenticationProvider {
+  /**
+   * @internal this method is meant for internal use only and may be changed without notice
+   */
   getAuthService(): AuthService
+
+  /**
+   * @internal this method is meant for internal use only and may be changed without notice
+   * Gives a new authentication provider for the same user and authentication method but for a different iCure group
+   * @param newGroupId id of the new group to switch to
+   * @param matches the list of groups the user is in, containing also the group-specific id of the user. Note that users with same login username but
+   * different password are considered as if they are different users and should not appear in these matches. You can get this list by calling
+   * {@link IccUserApi.getMatchingUsers} authenticated.
+   * @return a new authentication provider
+   */
+  switchGroup(newGroupId: string, matches: Array<UserGroup>): Promise<AuthenticationProvider>
 }
 
 export class EnsembleAuthenticationProvider implements AuthenticationProvider {
@@ -14,9 +32,16 @@ export class EnsembleAuthenticationProvider implements AuthenticationProvider {
   private jwtAuth: JwtAuthService
   private suspensionEnd: Date | undefined
 
-  constructor(private authApi: IccAuthApi, private username: string, private password: string, private jwtTimeout: number = 3600) {
-    this.jwtAuth = new JwtAuthService(this.authApi, this.username, this.password)
-    this.basicAuth = new BasicAuthService(this.username, this.password)
+  constructor(
+    private readonly authApi: IccAuthApi,
+    private readonly username: string,
+    private readonly password: string,
+    private readonly jwtTimeout: number = 3600,
+    jwtAuth?: JwtAuthService,
+    basicAuth?: BasicAuthService
+  ) {
+    this.jwtAuth = jwtAuth ?? new JwtAuthService(this.authApi, this.username, this.password)
+    this.basicAuth = basicAuth ?? new BasicAuthService(this.username, this.password)
   }
 
   getAuthService(): AuthService {
@@ -32,17 +57,34 @@ export class EnsembleAuthenticationProvider implements AuthenticationProvider {
       ? new EnsembleAuthService(null, new NoAuthService(), this.basicAuth)
       : new EnsembleAuthService(this.jwtAuth, new NoAuthService(), this.basicAuth)
   }
+
+  async switchGroup(newGroupId: string, matches: Array<UserGroup>): Promise<AuthenticationProvider> {
+    const switchInfo = await switchJwtAuth(this.authApi, this.jwtAuth, this.username, this.password, newGroupId, matches)
+    return new EnsembleAuthenticationProvider(
+      this.authApi,
+      switchInfo.loginForGroup,
+      this.password,
+      this.jwtTimeout,
+      switchInfo.switchedJwtAuth,
+      new BasicAuthService(switchInfo.loginForGroup, this.password)
+    )
+  }
 }
 
 export class JwtAuthenticationProvider implements AuthenticationProvider {
   private readonly jwtAuth: JwtAuthService
 
-  constructor(authApi: IccAuthApi, username: string, password: string) {
-    this.jwtAuth = new JwtAuthService(authApi, username, password)
+  constructor(private readonly authApi: IccAuthApi, private readonly username: string, private readonly password: string, jwtAuth?: JwtAuthService) {
+    this.jwtAuth = jwtAuth ?? new JwtAuthService(authApi, username, password)
   }
 
   getAuthService(): AuthService {
     return this.jwtAuth
+  }
+
+  async switchGroup(newGroupId: string, matches: Array<UserGroup>): Promise<AuthenticationProvider> {
+    const switchInfo = await switchJwtAuth(this.authApi, this.jwtAuth, this.username, this.password, newGroupId, matches)
+    return new JwtAuthenticationProvider(this.authApi, switchInfo.loginForGroup, this.password, switchInfo.switchedJwtAuth)
   }
 }
 
@@ -52,10 +94,53 @@ export class BasicAuthenticationProvider implements AuthenticationProvider {
   getAuthService(): AuthService {
     return new BasicAuthService(this.username, this.password)
   }
+
+  async switchGroup(newGroupId: string, matches: Array<UserGroup>): Promise<AuthenticationProvider> {
+    return Promise.resolve(new BasicAuthenticationProvider(loginForGroup(newGroupId, matches), this.password))
+  }
 }
 
 export class NoAuthenticationProvider implements AuthenticationProvider {
   getAuthService(): AuthService {
     return new NoAuthService()
   }
+
+  switchGroup(newGroupId: string, matches: Array<UserGroup>): Promise<AuthenticationProvider> {
+    return Promise.resolve(this)
+  }
+}
+
+/**
+ * @internal
+ */
+function loginForGroup(groupId: string, matches: Array<UserGroup>): string {
+  const matchForGroup = matches.find((x) => x.groupId === groupId)
+  if (!matchForGroup?.userId) {
+    throw new Error(`Can't switch to group ${groupId} for the current user.`)
+  }
+  return `${groupId}/${matchForGroup.userId}`
+}
+
+/**
+ * @internal
+ * Creates a jwtAuth from an existing one in order to switch group
+ */
+async function switchJwtAuth(
+  authApi: IccAuthApi,
+  jwtAuth: JwtAuthService,
+  username: string,
+  password: string,
+  newGroupId: string,
+  matches: Array<UserGroup>
+): Promise<{ loginForGroup: string; switchedJwtAuth: JwtAuthService }> {
+  const refreshToken = jwtAuth.isInErrorState() ? undefined : await jwtAuth.refreshToken
+  const switchedJwtInfo = refreshToken ? await authApi.switchGroup(refreshToken, newGroupId).catch(() => undefined) : undefined
+  const updatedLogin = loginForGroup(newGroupId, matches)
+  const switchedJwtAuth = new JwtAuthService(
+    authApi,
+    updatedLogin,
+    password,
+    switchedJwtInfo?.token && switchedJwtInfo?.refreshToken ? { authJwt: switchedJwtInfo.token, refreshJwt: switchedJwtInfo.refreshToken } : undefined
+  )
+  return { loginForGroup: updatedLogin, switchedJwtAuth }
 }
