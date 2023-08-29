ff65038f6a71787abc0819958f8e7dcb037a5ffd
Vincenzo Pierro <vcp@icure.com>
2023-08-29 13:54:25 +0200

    Support safe base 64

--- icc-api/model/ModelHelper.ts
+++ icc-api/model/ModelHelper.ts
@@ -64,22 +64,23 @@ export function b2a(a: string): string {
 }
 
 export function a2b(s: string): string {
+  const urlUnsafeString = s.replace(/_/g, '/').replace(/-/g, '+')
   if (typeof window !== 'undefined') {
     //Favour atob in browser
     if (typeof atob !== 'undefined') {
-      return atob(s)
+      return atob(urlUnsafeString)
     }
     if (typeof Buffer !== 'undefined') {
-      const buf = new Buffer(s, 'base64')
+      const buf = new Buffer(urlUnsafeString, 'base64')
       return buf.toString('latin1')
     }
   } else {
     if (typeof Buffer !== 'undefined') {
-      const buf = new Buffer(s, 'base64')
+      const buf = new Buffer(urlUnsafeString, 'base64')
       return buf.toString('latin1')
     }
     if (typeof atob !== 'undefined') {
-      return atob(s)
+      return atob(urlUnsafeString)
     }
   }
   throw new Error('Unsupported operation a2b')
