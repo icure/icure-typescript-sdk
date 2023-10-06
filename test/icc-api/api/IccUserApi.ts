85acb85a86b0ffa7e665565acacb4088e85bb118
Vincenzo Pierro <vcp@icure.com>
2023-10-06 16:29:57 +0200

    Added functions to add and remove roles from users

--- test/icc-api/api/IccUserApi.ts
+++ test/icc-api/api/IccUserApi.ts
@@ -4,9 +4,10 @@ import { getEnvironmentInitializer, hcp1Username, setLocalStorage, TestUtils } f
 import initApi = TestUtils.initApi
 import { getEnvVariables, TestVars } from '@icure/test-setup/types'
 import 'isomorphic-fetch'
+import initMasterApi = TestUtils.initMasterApi
 setLocalStorage(fetch)
 
-let env: TestVars | undefined
+let env: TestVars
 
 describe('User', () => {
   before(async function () {
@@ -16,9 +17,31 @@ describe('User', () => {
   })
 
   it('should be capable of creating a token', async () => {
-    const { userApi } = await initApi(env!, hcp1Username)
+    const { userApi } = await initApi(env, hcp1Username)
     const currentUser = await userApi.getCurrentUser()
     const token = await userApi.getTokenInGroup(currentUser.groupId!, currentUser.id!, `e2eTestTS-${randomUUID()}`, undefined, 3)
     expect(token.match(/[a-fA-F0-9]+/))
   }).timeout(30000)
+
+  it('should be able to add and remove a role to a user', async () => {
+    const { userApi, roleApi } = await initMasterApi(env)
+
+    const role = (await roleApi.getRoles())[0].id!
+
+    const login = `${randomUUID().substring(0, 6)}-test-user@icure.com`
+    const user = await userApi.createUser({
+      id: randomUUID(),
+      name: randomUUID(),
+      login: login,
+      email: login,
+    })
+    expect(user.roles?.length ?? 0).to.be.equal(0)
+
+    const userWithRole = await userApi.addRoles(user.id!, [role])
+    expect(userWithRole.roles?.length ?? 0).to.be.equal(1)
+    expect(userWithRole.roles![0]).to.be.equal(role)
+
+    const userWithoutRole = await userApi.removeRoles(user.id!, [role])
+    expect(userWithoutRole.roles?.length ?? 0).to.be.equal(0)
+  })
 })
