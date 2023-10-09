73d3a892b2e4e4aca6143debab49d030fb0ecc52
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-10-09 15:34:12 +0200

    Added IccRoleApi on V7 (#100)
    * Added IccRoleApi
    
    * Added functions to add and remove roles from users
    
    * Updated test
    
    * Added functions to get and set default roles
    
    * Update models.ts

---
+++ icc-api/api/IccRoleApi.ts
@@ -0,0 +1,41 @@
+import { XHR } from './XHR'
+import { AuthenticationProvider, NoAuthenticationProvider } from '../../icc-x-api'
+import { Role } from '../model/Role'
+import { iccRestApiPath } from './IccRestApiPath'
+
+export class IccRoleApi {
+  host: string
+  headers: Array<XHR.Header>
+  authenticationProvider: AuthenticationProvider
+  fetchImpl?: (input: RequestInfo, init?: RequestInit) => Promise<Response>
+
+  constructor(
+    host: string,
+    headers: any,
+    authenticationProvider?: AuthenticationProvider,
+    fetchImpl?: (input: RequestInfo, init?: RequestInit) => Promise<Response>
+  ) {
+    this.host = iccRestApiPath(host)
+    this.headers = Object.keys(headers).map((k) => new XHR.Header(k, headers[k]))
+    this.authenticationProvider = !!authenticationProvider ? authenticationProvider : new NoAuthenticationProvider()
+    this.fetchImpl = fetchImpl
+  }
+
+  handleError(e: XHR.XHRError): never {
+    throw e
+  }
+
+  /**
+   *
+   * @summary Gets all roles
+   */
+  getRoles(): Promise<Array<Role>> {
+    let _body = null
+
+    const _url = this.host + `/role` + '?ts=' + new Date().getTime()
+    let headers = this.headers
+    return XHR.sendCommand('GET', _url, headers, _body, this.fetchImpl, undefined, this.authenticationProvider.getAuthService())
+      .then((doc) => (doc.body as Array<JSON>).map((it) => new Role(it)))
+      .catch((err) => this.handleError(err))
+  }
+}
--- test/icc-api/api/IccUserApi.ts
+++ test/icc-api/api/IccUserApi.ts
@@ -4,9 +4,11 @@ import { getEnvironmentInitializer, hcp1Username, setLocalStorage, TestUtils } f
 import initApi = TestUtils.initApi
 import { getEnvVariables, TestVars } from '@icure/test-setup/types'
 import 'isomorphic-fetch'
+import initMasterApi = TestUtils.initMasterApi
+import { DatabaseInitialisation } from '@icure/apiV6'
 setLocalStorage(fetch)
 
-let env: TestVars | undefined
+let env: TestVars
 
 describe('User', () => {
   before(async function () {
@@ -16,9 +18,71 @@ describe('User', () => {
   })
 
   it('should be capable of creating a token', async () => {
-    const { userApi } = await initApi(env!, hcp1Username)
+    const { userApi } = await initApi(env, hcp1Username)
     const currentUser = await userApi.getCurrentUser()
     const token = await userApi.getTokenInGroup(currentUser.groupId!, currentUser.id!, `e2eTestTS-${randomUUID()}`, undefined, 3)
     expect(token.match(/[a-fA-F0-9]+/))
   }).timeout(30000)
+
+  it('should be able to add and remove a role to a user', async () => {
+    const { userApi, roleApi } = await initMasterApi(env)
+
+    const role = (await roleApi.getRoles())[0].id!
+
+    const login = `${randomUUID().substring(0, 6)}-test-user@icure.com`
+    const user = await userApi.createUser({
+      id: randomUUID(),
+      name: randomUUID(),
+      login: login,
+      email: login,
+    })
+    expect(user.roles?.length ?? 0).to.be.equal(0)
+
+    const userWithRole = await userApi.addRoles(user.id!, [role])
+    expect(userWithRole.roles?.length ?? 0).to.be.equal(1)
+    expect(userWithRole.roles![0]).to.be.equal(role)
+
+    const userWithoutRole = await userApi.removeRoles(user.id!, [role])
+    expect(userWithoutRole.roles?.length ?? 0).to.be.equal(0)
+  })
+
+  it('should be able to add and remove a role to a user in group', async () => {
+    const { userApi, roleApi, groupApi } = await initMasterApi(env)
+
+    const role = (await roleApi.getRoles())[0].id!
+
+    const groupId = randomUUID()
+    const groupName = groupId.substring(0, 5)
+    const groupPwd = randomUUID()
+    await groupApi.createGroup(
+      groupId,
+      groupName,
+      groupPwd,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      new DatabaseInitialisation({
+        users: [],
+        healthcareParties: [],
+      })
+    )
+
+    const login = `${randomUUID().substring(0, 6)}-test-user@icure.com`
+    const user = await userApi.createUserInGroup(groupId, {
+      id: randomUUID(),
+      name: randomUUID(),
+      login: login,
+      email: login,
+    })
+    expect(user.roles?.length ?? 0).to.be.equal(0)
+
+    const userWithRole = await userApi.addRolesInGroup(user.id!, groupId, [role])
+    expect(userWithRole.roles?.length ?? 0).to.be.equal(1)
+    expect(userWithRole.roles![0]).to.be.equal(role)
+
+    const userWithoutRole = await userApi.removeRolesInGroup(user.id!, groupId, [role])
+    expect(userWithoutRole.roles?.length ?? 0).to.be.equal(0)
+  })
 })
