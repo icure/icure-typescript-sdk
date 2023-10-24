21e05a9beb178bceda91fc6d2647912fd164a6c3
Luca Tremamunno <luca@icure.com>
2023-10-24 11:17:16 +0200

    Fix verification key reloading + test

--- icc-x-api/storage/KeyStorageImpl.ts
+++ icc-x-api/storage/KeyStorageImpl.ts
@@ -12,22 +12,28 @@ export class KeyStorageImpl implements KeyStorageFacade {
     return await this._storage.removeItem(key)
   }
 
+  private async getPartialKeyPair(key: string): Promise<{ publicKey?: JsonWebKey; privateKey?: JsonWebKey } | undefined> {
+    const loaded = await this._storage.getItem(key)
+    if (!loaded) return undefined
+    const keyPair = JSON.parse(loaded)
+    const res: { publicKey?: JsonWebKey; privateKey?: JsonWebKey } = {}
+    if (keyPair.hasOwnProperty('publicKey')) res['publicKey'] = keyPair['publicKey']
+    if (keyPair.hasOwnProperty('privateKey')) res['privateKey'] = keyPair['privateKey']
+    return res
+  }
+
   async getKeypair(key: string): Promise<{ publicKey: JsonWebKey; privateKey: JsonWebKey } | undefined> {
-    const keyPair = JSON.parse((await this._storage.getItem(key)) ?? '{}')
-    return keyPair.hasOwnProperty('publicKey') && keyPair.hasOwnProperty('privateKey')
-      ? {
-          publicKey: keyPair.publicKey as JsonWebKey,
-          privateKey: keyPair.privateKey as JsonWebKey,
-        }
-      : undefined
+    const partial = await this.getPartialKeyPair(key)
+    if (!partial || !partial.publicKey || !partial.privateKey) return undefined
+    return { publicKey: partial.publicKey, privateKey: partial.privateKey }
   }
 
   async getPrivateKey(key: string): Promise<JsonWebKey | undefined> {
-    return (await this.getKeypair(key))?.privateKey
+    return this.getPartialKeyPair(key).then((partial) => partial?.privateKey)
   }
 
   async getPublicKey(key: string): Promise<JsonWebKey | undefined> {
-    return (await this.getKeypair(key))?.publicKey
+    return this.getPartialKeyPair(key).then((partial) => partial?.publicKey)
   }
 
   async storeKeyPair(key: string, keyPair: { publicKey: JsonWebKey; privateKey: JsonWebKey }): Promise<void> {
--- test/utils/TestStorage.ts
+++ test/utils/TestStorage.ts
@@ -1,4 +1,4 @@
-import { hex2ua, KeyStorageFacade, pkcs8ToJwk, spkiToJwk, StorageFacade } from '../../icc-x-api'
+import { hex2ua, KeyStorageFacade, KeyStorageImpl, pkcs8ToJwk, spkiToJwk, StorageFacade } from '../../icc-x-api'
 import { KeyPair, ShaVersion } from '../../icc-x-api/crypto/RSA'
 import { StorageEntryKeysFactory } from '../../icc-x-api/storage/StorageEntryKeysFactory'
 import { DefaultStorageEntryKeysFactory } from '../../icc-x-api/storage/DefaultStorageEntryKeysFactory'
@@ -20,57 +20,8 @@ export class TestStorage implements StorageFacade<string> {
   }
 }
 
-export class TestKeyStorage implements KeyStorageFacade {
-  private readonly data = new Map<string, any>()
-
-  async deleteKeypair(key: string): Promise<void> {
-    this.data.delete(key)
+export class TestKeyStorage extends KeyStorageImpl {
+  constructor() {
+    super(new TestStorage())
   }
-
-  async getKeypair(key: string): Promise<{ publicKey: JsonWebKey; privateKey: JsonWebKey } | undefined> {
-    return this.data.get(key)
-  }
-
-  async getPrivateKey(key: string): Promise<JsonWebKey | undefined> {
-    return (await this.getKeypair(key))?.privateKey
-  }
-
-  async getPublicKey(key: string): Promise<JsonWebKey | undefined> {
-    return (await this.getKeypair(key))?.publicKey
-  }
-
-  async storeKeyPair(key: string, keyPair: { publicKey: JsonWebKey; privateKey: JsonWebKey }): Promise<void> {
-    this.data.set(key, keyPair)
-  }
-
-  async storePrivateKey(key: string, privateKey: JsonWebKey): Promise<void> {
-    this.data.set(key, privateKey)
-  }
-
-  async storePublicKey(key: string, publicKey: JsonWebKey): Promise<void> {
-    this.data.set(key, publicKey)
-  }
-}
-
-export async function testStorageWithKeys(
-  data: { dataOwnerId: string; pairs: { keyPair: KeyPair<string>; shaVersion: ShaVersion }[] }[]
-): Promise<{ keyStorage: KeyStorageFacade; storage: StorageFacade<string>; keyFactory: StorageEntryKeysFactory }> {
-  const keyStorage = new TestKeyStorage()
-  const keyFactory = new DefaultStorageEntryKeysFactory()
-  const storage = new TestStorage()
-  const icureStorage = new IcureStorageFacade(keyStorage, storage, keyFactory)
-  for (const { dataOwnerId, pairs } of data) {
-    for (const pair of pairs) {
-      await icureStorage.saveKey(
-        dataOwnerId,
-        pair.keyPair.publicKey.slice(-32),
-        {
-          privateKey: pkcs8ToJwk(hex2ua(pair.keyPair.privateKey)),
-          publicKey: spkiToJwk(hex2ua(pair.keyPair.publicKey), pair.shaVersion),
-        },
-        true
-      )
-    }
-  }
-  return { keyFactory, keyStorage, storage }
 }
