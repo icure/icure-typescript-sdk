8cafb8d7b6cef841bdf3d384b419e4cb4b2bd99c
Clément VANDENDAELEN <cv@icure.com>
2023-11-21 14:16:58 +0100

    Added getUserByPhoneNumber API endpoint
    Signed-off-by: Clément VANDENDAELEN <cv@icure.com>

--- test/icc-x-api/icc-user-x-api-test.ts
+++ test/icc-x-api/icc-user-x-api-test.ts
@@ -1,8 +1,14 @@
-import { getEnvironmentInitializer, hcp1Username, setLocalStorage } from '../utils/test_utils'
-import { before } from 'mocha'
-import { crypto } from '../../node-compat'
-import { TestApi } from '../utils/TestApi'
-import { getEnvVariables, TestVars } from '@icure/test-setup/types'
+import {getEnvironmentInitializer, hcp1Username, setLocalStorage, TestUtils} from '../utils/test_utils'
+import {before} from 'mocha'
+import {crypto} from '../../node-compat'
+import {TestApi} from '../utils/TestApi'
+import {getEnvVariables, TestVars} from '@icure/test-setup/types'
+import {CryptoPrimitives} from "../../icc-x-api"
+import {webcrypto} from "crypto"
+import {expect} from "chai"
+import {User} from "../../icc-api/model/User"
+import {v4 as uuid} from "uuid"
+import initMasterApi = TestUtils.initMasterApi
 
 setLocalStorage(fetch)
 let env: TestVars
@@ -18,5 +24,29 @@ describe('icc-x-user-api Tests', () => {
     // Given
     const api = await TestApi(env.iCureUrl, env.dataOwnerDetails[hcp1Username].user, env.dataOwnerDetails[hcp1Username].password, crypto)
     await api.userApi.getCurrentUser()
+  })
+
+  it('Can get an user by its phoneNumber', async () => {
+    const primitives = new CryptoPrimitives(webcrypto as any)
+    const phoneNumber = `+${primitives.randomUuid()}`
+
+    const api = await initMasterApi(env)
+
+    const createdUser = await api.userApi.createUser(
+      new User({
+        id: uuid(),
+        name: `user-${primitives.randomUuid()}`,
+        login: `user-${primitives.randomUuid()}`,
+        email: `user-${primitives.randomUuid()}@icure.com`,
+        mobilePhone: phoneNumber,
+      })
+    )
+
+    const user = await api.userApi.getUserByPhoneNumber(phoneNumber)
+
+    expect(user).to.be.deep.equal({
+      ...createdUser,
+      systemMetadata: user.systemMetadata
+    })
   })
 })
