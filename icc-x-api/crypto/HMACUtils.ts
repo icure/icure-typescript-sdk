ef3defb231ad404a968674a8347f54f13d6e1e4c
Luca Tremamunno <luca@icure.com>
2024-01-25 11:50:14 +0100

    Add hmac key size checks

--- icc-x-api/crypto/HMACUtils.ts
+++ icc-x-api/crypto/HMACUtils.ts
@@ -1,9 +1,10 @@
 export class HMACUtils {
   private readonly _crypto: Crypto
+  private readonly recommendedKeyLengthBytes = 128
   private readonly params: HmacKeyGenParams = {
     name: 'HMAC',
     hash: 'SHA-512',
-    length: 128 * 8, // Recommended length in bits. Adding this because not all implementations behave well.
+    length: this.recommendedKeyLengthBytes * 8, // Recommended length in bits. Adding this because not all implementations behave well.
   }
 
   constructor(crypto: Crypto) {
@@ -11,7 +12,12 @@ export class HMACUtils {
   }
 
   async generateKey(): Promise<CryptoKey> {
-    return this._crypto.subtle.generateKey({ ...this.params }, true, ['sign', 'verify'])
+    const generatedKey = await this._crypto.subtle.generateKey({ ...this.params }, true, ['sign', 'verify'])
+    const exportedKey = await this._crypto.subtle.exportKey('raw', generatedKey)
+    if (exportedKey.byteLength !== this.recommendedKeyLengthBytes) {
+      throw new Error(`Generated key has unexpected length - expected ${this.recommendedKeyLengthBytes} bytes, got ${exportedKey.byteLength} bytes`)
+    }
+    return generatedKey
   }
 
   async exportKey(key: CryptoKey): Promise<ArrayBuffer> {
@@ -19,6 +25,9 @@ export class HMACUtils {
   }
 
   async importKey(key: ArrayBuffer): Promise<CryptoKey> {
+    if (key.byteLength !== this.recommendedKeyLengthBytes) {
+      throw new Error(`Key has unexpected length - expected ${this.recommendedKeyLengthBytes} bytes, got ${key.byteLength} bytes`)
+    }
     return this._crypto.subtle.importKey('raw', key, { ...this.params }, true, ['sign', 'verify'])
   }
 
