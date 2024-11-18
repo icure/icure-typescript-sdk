3f06a2e7fe6e6deb8afa2cce32795450769548aa
Vincenzo Claudio Pierro <vcp@icure.com>
2024-11-18 14:35:26 +0100

    Fixed cache on icc-hcparty-x-api (#163)
    * Fixed Cache on icc-hcparty-x-api
    
    * Updates after PR review
    
    * Updates after PR review

--- icc-x-api/icc-hcparty-x-api.ts
+++ icc-x-api/icc-hcparty-x-api.ts
@@ -72,16 +72,9 @@ export class IccHcpartyXApi extends IccHcpartyApi {
     return finalHcParty
   }
 
-  putHcPartyInCache(key: string, value: Promise<HealthcareParty> | null = null): Promise<HealthcareParty> {
-    const hcp =
-      value ||
-      super.getHealthcareParty(key).catch((e) => {
-        console.log(`Evict key ${key} because of error`)
-        delete this.hcPartyCache[key]
-        throw e
-      })
-    this.hcPartyCache[key] = [Date.now() + this.CACHE_RETENTION_IN_MS, hcp]
-    return hcp
+  putHcPartyInCache(key: string, hcpPromise: Promise<HealthcareParty>): Promise<HealthcareParty> {
+    this.hcPartyCache[key] = [Date.now() + this.CACHE_RETENTION_IN_MS, hcpPromise]
+    return hcpPromise
   }
 
   createHealthcareParty(body?: HealthcareParty): Promise<HealthcareParty> {
@@ -101,24 +94,44 @@ export class IccHcpartyXApi extends IccHcpartyApi {
     return super.modifyHealthcareParty(this.completeNames(body)).then((hcp) => this.putHcPartyInCache(hcp.id!, Promise.resolve(hcp)))
   }
 
-  getHealthcareParty(healthcarePartyId: string, bypassCache = false): Promise<HealthcareParty | any> {
+  async getHealthcareParty(healthcarePartyId: string, bypassCache = false): Promise<HealthcareParty | any> {
     const fromCache = bypassCache ? undefined : this.getHcPartyFromCache(healthcarePartyId)
-    return fromCache || this.putHcPartyInCache(healthcarePartyId)
+    if (fromCache) {
+      try {
+        return await fromCache
+      } catch {
+        if (this.hcPartyCache[healthcarePartyId][1] === fromCache) {
+          delete this.hcPartyCache[healthcarePartyId]
+        }
+        return this.getHealthcareParty(healthcarePartyId, bypassCache)
+      }
+    } else {
+      return await this.putHcPartyInCache(healthcarePartyId, super.getHealthcareParty(healthcarePartyId))
+    }
   }
 
   getHealthcarePartyHierarchyIds(healthcarePartyId: string, bypassCache = false): Promise<string[]> {
-    const fromCache = bypassCache ? undefined : this.getHcPartyFromCache(healthcarePartyId)
-    return (fromCache || this.putHcPartyInCache(healthcarePartyId)).then(async (hcp: HealthcareParty) => {
+    return this.getHealthcareParty(healthcarePartyId, bypassCache).then(async (hcp: HealthcareParty) => {
       return hcp ? (hcp.parentId ? (await this.getHealthcarePartyHierarchyIds(hcp.parentId!, bypassCache)).concat([hcp.id!]) : [hcp.id!]) : []
     })
   }
 
-  getHealthcareParties(healthcarePartyIds: ListOfIds): Promise<Array<HealthcareParty> | any> {
+  async getHealthcareParties(healthcarePartyIds: ListOfIds): Promise<Array<HealthcareParty> | any> {
     const ids = healthcarePartyIds.ids
     if (!ids || !ids.length) {
       return Promise.resolve([])
     }
-    const cached: Array<[string, Promise<HealthcareParty> | null]> = ids.map((id) => [id, this.getHcPartyFromCache(id)])
+    const cached: Array<[string, HealthcareParty | null]> = []
+    for (const id of ids) {
+      let hcp
+      try {
+        hcp = await this.getHcPartyFromCache(id)
+      } catch {
+        hcp = null
+        delete this.hcPartyCache[id]
+      }
+      cached.push([id, hcp])
+    }
     const toFetch = cached.filter((x) => !x[1]).map((x) => x[0])
 
     if (!toFetch.length) {
@@ -129,10 +142,17 @@ export class IccHcpartyXApi extends IccHcpartyApi {
     return Promise.all(
       cached.map(
         (x) =>
-          x[1] ||
+          x[1] ??
           this.putHcPartyInCache(
             x[0],
-            prom.then((hcps) => hcps.find((h) => h.id === x[0])!)
+            prom.then((hcps) => {
+              const hcp = hcps.find((h) => h.id === x[0])
+              if (!!hcp) {
+                return hcp
+              } else {
+                throw new Error(`Hcp with id ${x[0]} not found`)
+              }
+            })
           )
       )
     )
