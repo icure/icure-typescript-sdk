fda314e14cf65db082df2776c28895062752c806
Luca Tremamunno <tremaluca96@gmail.com>
2025-02-18 12:14:23 +0100

    Allow injection at initialization

--- icc-x-api/index.ts
+++ icc-x-api/index.ts
@@ -256,6 +256,22 @@ export interface IcureApiOptions {
    * Set it to true only for connecting to kraken-lite versions < 0.1.187
    */
   readonly useLiteCompatibilityMode?: boolean
+  /**
+   * Allows injecting exchange data similarly to {@link IccCryptoXApi.injectExchangeData} but directly on
+   * initialization.
+   * When possible, this solution should be preferred as it allows minimizing proliferation of exchange data under
+   * certain circumstances.
+   */
+  readonly injectExchangeData?: {
+    details: {
+      exchangeDataId: string
+      accessControlSecret: ArrayBuffer
+      exchangeKey: ArrayBuffer
+      sharedSignatureKey: ArrayBuffer
+      verified: boolean
+    }[]
+    reEncryptWithOwnKeys: boolean
+  }
 }
 
 namespace IcureApiOptions {
@@ -274,6 +290,7 @@ namespace IcureApiOptions {
     readonly encryptedFieldsConfig: EncryptedFieldsConfig
     readonly groupSelector: (availableGroupsInfo: UserGroup[]) => Promise<string>
     readonly disableParentKeysInitialisation: boolean
+    readonly injectExchangeData: IcureApiOptions['injectExchangeData'] | undefined
 
     constructor(custom: IcureApiOptions) {
       this.entryKeysFactory = custom.entryKeysFactory ?? Defaults.entryKeysFactory
@@ -284,6 +301,7 @@ namespace IcureApiOptions {
       this.encryptedFieldsConfig = custom.encryptedFieldsConfig ?? {}
       this.groupSelector = custom.groupSelector ?? ((groups) => Promise.resolve(groups[0].groupId!))
       this.disableParentKeysInitialisation = custom.disableParentKeysInitialisation ?? false
+      this.injectExchangeData = custom.injectExchangeData
     }
   }
 }
@@ -778,6 +796,9 @@ async function initialiseCryptoWithProvider(
     cryptoPrimitives,
     !params.disableParentKeysInitialisation
   )
+  if (params.injectExchangeData != null && params.injectExchangeData.details.length > 0) {
+    await exchangeDataManager.injectDecryptedExchangeData(params.injectExchangeData.details, params.injectExchangeData.reEncryptWithOwnKeys)
+  }
   const exchangeDataMapManager = new ExchangeDataMapManager(
     new IccExchangeDataMapApi(host, updatedHeaders, groupSpecificAuthenticationProvider, fetchImpl)
   )
