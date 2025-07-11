ac0338c960f5af12ba6646868af925a265b60eab
Clément Vandendaelen <cv@icure.com>
2025-07-11 15:49:25 +0200

    fix: Fixed CSM-584 (#188)
    * fix: Fixed CSM-584
    
    * fix: Avoid recomputing uppercased ISO values

--- icc-api/model/Service.ts
+++ icc-api/model/Service.ts
@@ -15,6 +15,7 @@ import { Content } from './Content'
 import { Delegation } from './Delegation'
 import { Identifier } from './Identifier'
 import { SecurityMetadata } from './SecurityMetadata'
+import {ISO639_1_VALUES_ARRAY} from "./ISO639_1"
 
 /**
  * This entity represents a Service. A Service is created in the course a contact. Services include subjective information provided by the patient, such as complaints, reason for visit, feelings, etc. or objective information like bio-metric measures (blood pressure, temperature, heart beat, etc.), or physical exam description, diagnosis, prescription, integration of lab reports from another healthcare party, action plan, etc. Any action performed by the healthcare party which is relevant for the healthcare element of a patient is considered a service. The services can be linked to healthcare elements or other structuring elements of the medical record
@@ -27,8 +28,20 @@ export class Service {
     )
 
     if (!!this.content) {
+      const uppercasedIso639_1 = ISO639_1_VALUES_ARRAY.map((e) => e.toUpperCase())
       this.content = Object.fromEntries(
-        Object.entries(this.content).map(([key, value]) => [key, new Content(value)])
+        Object.entries(
+          this.content).map(([key, value]) => {
+            const isUppercasedIso = uppercasedIso639_1.includes(key)
+
+            let contentKey = key
+            if (isUppercasedIso) {
+              contentKey = key.toLowerCase()
+            }
+
+            return [contentKey, new Content(value)]
+          }
+        )
       )
     }
   }
