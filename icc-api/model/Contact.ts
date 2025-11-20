3a191d5614eccc1dcabde7ddef621b5f8849230a
Clément Vandendaelen <cv@icure.com>
2025-11-20 16:24:32 +0100

    feat: Added Contact's participantList (#190)

--- icc-api/model/Contact.ts
+++ icc-api/model/Contact.ts
@@ -17,6 +17,7 @@ import { SubContact } from './SubContact'
 import { SecurityMetadata } from './SecurityMetadata'
 import {Annotation} from "./Annotation"
 import {Address} from "./Address"
+import { ContactParticipant } from './ContactParticipant'
 
 /**
  * This entity is a root-level object. It represents a contact. It is serialized in JSON and saved in the underlying icure-contact CouchDB database. The contact is the entity that records the medical information about the patient chronologically. A visit to the patient's house, a consultation at the practice, a phone call between the patient and the healthcare party or integrating lab reports into the medical file are examples of when a contact can be recorded. A contact can occur with or without direct interaction between the patient and the healthcare party. For example, when a healthcare party encodes data received from laboratory's test result, this is done in the absence of a patient. A contact groups together pieces of information collected during one single event, for one single patient and for one or more healthcare parties. Patient's complaints, the diagnosis of a new problem, a surgical procedure, etc. are collected inside a contact. The main sub-element of the contact is the service. Each atomic piece of information collected during a contact is a service and is stored inside the services list of a contact.
@@ -120,8 +121,10 @@ export class Contact {
   services?: Array<Service>
   /**
    * The participants to the contact. The key is the type of participant, the value is the id of the participant data owner id
+   * @deprecated use participantList
    */
   participants?: Record<ParticipantType, string>
+  participantList?: Array<ContactParticipant>
   healthcarePartyId?: string
   modifiedContactId?: string
   /**
@@ -165,3 +168,4 @@ export type ParticipantType =
   | 'translator'
   | 'emergency'
   | 'location'
+  | 'recorder'
